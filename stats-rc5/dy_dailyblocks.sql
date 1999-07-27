063b54c773430f548665136237920f8248c041b4
nugget <nugget>
1999-07-27 21:37:27 +0000

    CACHE_dailyblocks is no longer in use.

--- stats-rc5/dy_dailyblocks.sql
+++ stats-rc5/dy_dailyblocks.sql
@@ -1,24 +1,19 @@
-# $Id: dy_dailyblocks.sql,v 1.1 1999/07/27 20:49:03 nugget Exp $
+# $Id: dy_dailyblocks.sql,v 1.2 1999/07/27 21:37:27 nugget Exp $
 
-drop table CACHE_dailyblocks
-go
-
-create table CACHE_dailyblocks
-(
-  date smalldatetime,
-  blocks numeric(12,0)
-)
-go
-
-insert into CACHE_dailyblocks (date, blocks)
-select distinct date, sum(blocks)
-from RC5_64_master
-group by date
-order by date
-go
-
-create index date on CACHE_dailyblocks(date)
-go
-
-grant select on CACHE_dailyblocks to public
+insert into STATS_dailies (date, blocks,
+                           participants, top_oparticipant, top_opblocks, top_yparticipant, top_yblocks,
+                           teams, top_oteam, top_otblocks, top_yteam, top_yblocks)
+select 
+  (select max(date) from RC5_64_master) as date,
+  (select sum(blocks) from RC5_64_master where date = (select max(date) from RC5_64_master)) as blocks,
+  (select count(*) from RC5_64_master where date = (select max(date) from RC5_64_master)) as participants,
+  (select id from stats.statproc.CACHE_em_RANK where Rank = 1) as top_oparticpant,
+  (select blocks from stats.statproc.CACHE_em_RANK where Rank = 1) as top_opblocks,
+  (select id from stats.statproc.CACHE_em_YRANK where Rank = 1) as top_yparticpant,
+  (select blocks from stats.statproc.CACHE_em_YRANK where Rank = 1) as top_ypblocks,
+  (select count(team) from stats.statproc.CACHE_tm_YRANK) as teams,
+  (select team from stats.statproc.CACHE_tm_RANK where Rank = 1) as top_oteam,
+  (select blocks from stats.statproc.CACHE_tm_RANK where Rank = 1) as top_otblocks,
+  (select team from stats.statproc.CACHE_tm_YRANK where Rank = 1) as top_yteam,
+  (select blocks from stats.statproc.CACHE_tm_YRANK where Rank = 1) as top_ytblocks
 go
