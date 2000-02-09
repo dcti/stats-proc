097e85a042faab9e9566530e48b916618bfa8d44
nugget <nugget>
2000-02-09 16:17:04 +0000

    Moving this backup directory to the Attic.

--- daily/backup/dy_maxdate.sql
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/sqsh -i
-#
-# $Id: dy_maxdate.sql,v 1.1 2000/02/09 16:13:58 nugget Exp $
-
-select convert(char(8),max(date),112) as maxdate from ${1}_master 
-# turn off header and rows affected output
-go -f -h
-
