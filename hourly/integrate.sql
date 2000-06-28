b18360e3df0c364fb07788442681a0cc20976774
decibel <decibel>
2000-06-28 00:30:52 +0000

    Modified to assign participant ID early on.

--- hourly/integrate.sql
+++ hourly/integrate.sql
@@ -1,6 +1,6 @@
 #!/usr/bin/sqsh -i
 #
-# $Id: integrate.sql,v 1.4 2000/06/26 20:13:56 decibel Exp $
+# $Id: integrate.sql,v 1.4.2.1 2000/06/28 00:30:52 decibel Exp $
 #
 # Move data from the import_bcp table to the daytables
 #
@@ -60,23 +60,69 @@ password assign automatic
 */
 create table #Email_Contrib_Today
 (
-	EMAIL varchar (64) NULL,
-	WORK_UNITS numeric(20, 0) NULL
+	EMAIL		varchar (64)	not NULL,
+	ID		int		not NULL,
+	WORK_UNITS	numeric(20, 0)	not NULL
+)
+create table #dayemails
+(
+	ID		numeric(10, 0)	identity,
+	EMAIL		varchar(64)	not NULL
 )
 go
 /* Put EMAIL data into temp table */
 /* First, put the latest set of logs in */
-insert #Email_Contrib_Today (EMAIL, WORK_UNITS)
-	select EMAIL, sum(WORK_UNITS)
+insert #Email_Contrib_Today (EMAIL, ID, WORK_UNITS)
+	select EMAIL, 0, sum(WORK_UNITS)
 	from import_bcp
 	group by EMAIL
 
+/* Assign ID's for everyone who has an ID */
+-- NOTE: At some point we might want to set TEAM_ID and CREDIT_ID here as well
+update #Email_Contrib_Today
+	set ID = sp.ID
+	from STATS_Participant sp
+	where sp.EMAIL = Email_Contrib_Today.EMAIL
+go
+
+/* Add new participants to STATS_Participant */
+
+/* First, copy all new participants to #dayemails to do the identity assignment */
+insert #dayemails (EMAIL)
+	select distinct EMAIL
+	from #Email_Contrib_Today
+	where ID = 0
+	order by EMAIL
+go
+
+/* Figure out where to start assigning at */
+declare @idoffset int
+select @idoffset = max(id)
+	from STATS_Participant
+
+-- [BW] If we switch to retire_to = id as the normal condition,
+--	this insert should insert (id, EMAIL, retire_to)
+--	from ID + @idoffset, EMAIL, ID + @idoffset
+insert into STATS_participant (ID, EMAIL)
+	select ID + @idoffset, EMAIL
+	from #dayemails
+
+/* Assign the new IDs to the new participants */
+-- JCN: where Email_Contrib_Today might be faster here...
+update #Email_Contrib_Today
+	set ID = sp.ID
+	from STATS_Participant sp, #dayemails de
+	where sp.EMAIL = Email_Contrib_Today.EMAIL
+		and sp.EMAIL = de.EMAIL
+		and de.EMAIL = Email_Contrib_Today.EMAIL
+go
+
 /* Now, add the stuff from the previous hourly runs */
-insert #Email_Contrib_Today (EMAIL, WORK_UNITS)
-	select EMAIL, sum(WORK_UNITS)
+-- JCN: Removed sum() and group by.. data in Email_Contrib_Today should be summed already
+insert #Email_Contrib_Today (EMAIL, ID, WORK_UNITS)
+	select "", ID, WORK_UNITS
 	from Email_Contrib_Today
 	where PROJECT_ID = ${1}
-	group by EMAIL
 
 /* Finally, remove the previous records from Email_Contrib_Today and insert the new
 ** data from the temp table. (It seems there should be a better way to do this...)
@@ -85,8 +131,8 @@ begin transaction
 delete Email_Contrib_Today
 	where PROJECT_ID = ${1}
 
-insert into Email_Contrib_Today (PROJECT_ID, EMAIL, WORK_UNITS, ID, TEAM_ID, CREDIT_ID)
-	select ${1}, EMAIL, sum(WORK_UNITS), 0, 0, 0
+insert into Email_Contrib_Today (PROJECT_ID, WORK_UNITS, ID, TEAM_ID, CREDIT_ID)
+	select ${1}, sum(WORK_UNITS), ID, 0, 0
 	from #Email_Contrib_Today
 	group by EMAIL
 commit transaction
