6f28b380be07388dddf77dbbaf25c2edb452d179
decibel <decibel>
2000-07-14 23:24:23 +0000

    Changed so that we properly don't count black-balled participants and teams.

--- daily/dy_dailyblocks.sql
+++ daily/dy_dailyblocks.sql
@@ -1,6 +1,6 @@
 #!/usr/bin/sqsh -i
 #
-# $Id: dy_dailyblocks.sql,v 1.8 2000/06/28 10:59:02 decibel Exp $
+# $Id: dy_dailyblocks.sql,v 1.9 2000/07/14 23:24:23 decibel Exp $
 #
 # Inserts the daily totals
 #
@@ -22,16 +22,33 @@ insert Daily_Summary (DATE, PROJECT_ID, WORK_UNITS,
 		0, 0, 0, 0, 0, 0,
 		0, 0, 0, 0, 0, 0)
 
-select @work = sum(WORK_UNITS),
-		@count = count(*)
+--
+-- Total Work Units
+--
+select @work = sum(WORK_UNITS)
 	from Email_Contrib_Today
 	where PROJECT_ID = ${1}
 update Daily_Summary
-	set WORK_UNITS = @work,
-		PARTICIPANTS = @count
+	set WORK_UNITS = @work
 	where Daily_Summary.date = @stats_date
 		and Daily_Summary.PROJECT_ID = ${1}
 
+--
+-- Number of Participants
+--
+select @count = count(distinct ect.CREDIT_ID)
+	from Email_Contrib_Today ect, STATS_Participant sp
+	where ect.CREDIT_ID = sp.ID
+		and sp.listmode < 9
+		and ect.PROJECT_ID = ${1}
+update Daily_Summary
+	set PARTICIPANTS = @count
+	where Daily_Summary.date = @stats_date
+		and Daily_Summary.PROJECT_ID = ${1}
+
+--
+-- Number of New Participants
+--
 select @count = count(*)
 	from Email_Rank
 		where FIRST_DATE = @stats_date
@@ -41,15 +58,23 @@ update Daily_Summary
 	where Daily_Summary.date = @stats_date
 		and Daily_Summary.PROJECT_ID = ${1}
 
+--
+-- Number of Teams
+--
 select @count = count(distinct TEAM_ID)
-	from Email_Contrib_Today
-	where TEAM_ID >= 1
-		and PROJECT_ID = ${1}
+	from Email_Contrib_Today ect, STATS_Team st
+	where ect.TEAM_ID >= 1
+		and ect.TEAM_ID = st.team
+		and st.LISTMODE < 10
+		and ect.PROJECT_ID = ${1}
 update Daily_Summary
 	set teams = @count
 	where Daily_Summary.date = @stats_date
 		and Daily_Summary.PROJECT_ID = ${1}
 
+--
+-- Number of New Teams
+--
 select @count = count(*)
 	from Team_Rank
 		where FIRST_DATE = @stats_date
@@ -59,6 +84,9 @@ update Daily_Summary
 	where Daily_Summary.date = @stats_date
 		and Daily_Summary.PROJECT_ID = ${1}
 
+--
+-- Top Participant Info, Overall
+--
 update Daily_Summary
 	set TOP_OPARTICIPANT = r.ID,
 		TOP_OPWORK = r.WORK_TOTAL
@@ -68,6 +96,9 @@ update Daily_Summary
 		and r.PROJECT_ID = ${1}
 		and r.OVERALL_RANK = 1
 
+--
+-- Top Participant Info for Yesterday
+--
 update Daily_Summary
 	set TOP_YPARTICIPANT = r.ID,
 		TOP_YPWORK = r.WORK_TODAY
@@ -77,6 +108,9 @@ update Daily_Summary
 		and r.PROJECT_ID = ${1}
 		and r.DAY_RANK = 1
 
+--
+-- Top Team Info, Overall
+--
 update Daily_Summary
 	set TOP_OTEAM = r.TEAM_ID,
 		TOP_OTWORK = r.WORK_TOTAL
@@ -86,6 +120,9 @@ update Daily_Summary
 		and r.PROJECT_ID = ${1}
 		and r.OVERALL_RANK = 1
 
+--
+-- Top Team Info for Yesterday
+--
 update Daily_Summary
 	set TOP_YTEAM = r.TEAM_ID,
 		TOP_YTWORK = r.WORK_TODAY
