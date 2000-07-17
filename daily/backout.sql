8d4b20fa4125a3485b597987a8e7eccf5d637d5c
decibel <decibel>
2000-07-17 13:11:50 +0000

    Fixed some quoting bugs.

--- daily/backout.sql
+++ daily/backout.sql
@@ -1,6 +1,6 @@
 #!/usr/bin/sqsh -i
 #
-# $Id: backout.sql,v 1.1 2000/07/17 13:02:17 decibel Exp $
+# $Id: backout.sql,v 1.2 2000/07/17 13:11:50 decibel Exp $
 #
 # This script will back out all stats data to a given date
 #
@@ -9,19 +9,19 @@
 #	The date to back out *to*. The data specified will *remain* in the database.
 
 begin transaction
-print "Deleting from Email_Contrib where DATE > '%1!'", ${2}
+print "Deleting from Email_Contrib where DATE > '%1!'", "${2}"
 go
-delete from Email_Contrib where PROJECT_ID = ${1} and DATE > '${2}'
-go
-
-print "Deleting from Platform_Contrib where DATE > '%1!'", ${2}
-go
-delete from Platform_Contrib where PROJECT_ID = ${1} and DATE > '${2}'
+delete from Email_Contrib where PROJECT_ID = ${1} and DATE > "${2}"
 go
 
-print "Deleting from Daily_Summary where DATE > '%1!'", ${2}
+print "Deleting from Platform_Contrib where DATE > '%1!'", "${2}"
 go
-delete Daily_Summary where PROJECT_ID = ${1} and DATE > '${2}'
+delete from Platform_Contrib where PROJECT_ID = ${1} and DATE > "${2}"
+go
+
+print "Deleting from Daily_Summary where DATE > '%1!'", "${2}"
+go
+delete Daily_Summary where PROJECT_ID = ${1} and DATE > "${2}"
 go
 
 print "Deleting from Email_Rank"
@@ -47,7 +47,7 @@ insert into Email_Rank (PROJECT_ID, ID, FIRST_DATE, LAST_DATE, WORK_TODAY, WORK_
 		DAY_RANK, DAY_RANK_PREVIOUS, OVERALL_RANK, OVERALL_RANK_PREVIOUS
 	from Email_Rank_Backup
 	where PROJECT_ID = ${1}
-		and BACKUP_DATE = '${2}'
+		and BACKUP_DATE = "${2}"
 go
 
 print "Inserting into Team_Rank"
@@ -60,7 +60,7 @@ insert into Team_Rank (PROJECT_ID, TEAM_ID, FIRST_DATE, LAST_DATE, WORK_TODAY, W
 		MEMBERS_TODAY, MEMBERS_OVERALL, MEMBERS_CURRENT
 	from Team_Rank_Backup
 	where PROJECT_ID = ${1}
-		and BACKUP_DATE = '${2}'
+		and BACKUP_DATE = "${2}"
 go
 
 print "Inserting into Team_Members"
@@ -71,6 +71,6 @@ insert into Team_Members (PROJECT_ID, ID, TEAM_ID, FIRST_DATE, LAST_DATE, WORK_T
 		DAY_RANK, DAY_RANK_PREVIOUS, OVERALL_RANK, OVERALL_RANK_PREVIOUS
 	from Team_Members_Backup
 	where PROJECT_ID = ${1}
-		and BACKUP_DATE = '${2}'
+		and BACKUP_DATE = "${2}"
 go
 commit transaction
