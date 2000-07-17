e4ca0b7037e1dea69136de36e900157b9eeb0b77
decibel <decibel>
2000-07-17 13:13:13 +0000

    Added 'statproc.' where needed.

--- daily/backout.sql
+++ daily/backout.sql
@@ -1,12 +1,15 @@
 #!/usr/bin/sqsh -i
 #
-# $Id: backout.sql,v 1.2 2000/07/17 13:11:50 decibel Exp $
+# $Id: backout.sql,v 1.3 2000/07/17 13:13:13 decibel Exp $
 #
 # This script will back out all stats data to a given date
 #
 # Arguments
 #	Project ID
 #	The date to back out *to*. The data specified will *remain* in the database.
+
+use stats
+go
 
 begin transaction
 print "Deleting from Email_Contrib where DATE > '%1!'", "${2}"
@@ -45,7 +48,7 @@ insert into Email_Rank (PROJECT_ID, ID, FIRST_DATE, LAST_DATE, WORK_TODAY, WORK_
 		DAY_RANK, DAY_RANK_PREVIOUS, OVERALL_RANK, OVERALL_RANK_PREVIOUS)
 	select ${1}, ID, FIRST_DATE, LAST_DATE, WORK_TODAY, WORK_TOTAL,
 		DAY_RANK, DAY_RANK_PREVIOUS, OVERALL_RANK, OVERALL_RANK_PREVIOUS
-	from Email_Rank_Backup
+	from statproc.Email_Rank_Backup
 	where PROJECT_ID = ${1}
 		and BACKUP_DATE = "${2}"
 go
@@ -58,7 +61,7 @@ insert into Team_Rank (PROJECT_ID, TEAM_ID, FIRST_DATE, LAST_DATE, WORK_TODAY, W
 	select ${1}, TEAM_ID, FIRST_DATE, LAST_DATE, WORK_TODAY, WORK_TOTAL,
 		DAY_RANK, DAY_RANK_PREVIOUS, OVERALL_RANK, OVERALL_RANK_PREVIOUS,
 		MEMBERS_TODAY, MEMBERS_OVERALL, MEMBERS_CURRENT
-	from Team_Rank_Backup
+	from statproc.Team_Rank_Backup
 	where PROJECT_ID = ${1}
 		and BACKUP_DATE = "${2}"
 go
@@ -69,7 +72,7 @@ insert into Team_Members (PROJECT_ID, ID, TEAM_ID, FIRST_DATE, LAST_DATE, WORK_T
 		DAY_RANK, DAY_RANK_PREVIOUS, OVERALL_RANK, OVERALL_RANK_PREVIOUS)
 	select ${1}, ID, TEAM_ID, FIRST_DATE, LAST_DATE, WORK_TODAY, WORK_TOTAL,
 		DAY_RANK, DAY_RANK_PREVIOUS, OVERALL_RANK, OVERALL_RANK_PREVIOUS
-	from Team_Members_Backup
+	from statproc.Team_Members_Backup
 	where PROJECT_ID = ${1}
 		and BACKUP_DATE = "${2}"
 go
