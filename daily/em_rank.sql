5cd31bcfd735d21932007ad6ae6ec4536b131c77
decibel <decibel>
2000-10-04 21:54:28 +0000

    Re-organized things a bit. Build all the temporary tables first, then make only one pass through Email_Rank. Simplified the update for that pass, too.

--- daily/em_rank.sql
+++ daily/em_rank.sql
@@ -1,7 +1,7 @@
 #!/usr/bin/sqsh -i
 /*
 #
-# $Id: em_rank.sql,v 1.10 2000/09/22 02:41:37 decibel Exp $
+# $Id: em_rank.sql,v 1.11 2000/10/04 21:54:28 decibel Exp $
 #
 # Does the participant ranking (overall)
 #
@@ -11,6 +11,7 @@
 
 use stats
 set rowcount 0
+set flushmessage on
 go
 print '!! Begin e-mail ranking'
 print ' Drop indexes on Email_Rank'
@@ -19,7 +20,7 @@ go
 --drop index Email_Rank.iOVERALL_RANK
 --go
 
-print ' Rank all, today'
+print ' Create rank table for today'
 go
 create table #rank_assign
 (
@@ -35,36 +36,23 @@ insert #rank_assign (ID, WORK_UNITS)
 	order by WORK_TODAY desc, ID desc
 go
 
-create clustered index iWORK_UNITS on #rank_assign(WORK_UNITS)
-go
-
-create table #rank_tie
+create table #rank_tie_today
 (
 	WORK_UNITS numeric(20, 0),
 	rank int
 )
 go
-insert #rank_tie
+insert #rank_tie_today
 	select WORK_UNITS, min(IDENT)
 	from #rank_assign
 	group by WORK_UNITS
 go
 
-create clustered index iWORK_UNITS on #rank_tie(WORK_UNITS)
-go
-
-update Email_Rank
-	set DAY_RANK = #rank_tie.rank
-	from #rank_tie, #rank_assign
-	where #rank_tie.WORK_UNITS = #rank_assign.WORK_UNITS
-		and Email_Rank.ID = #rank_assign.ID
-		and Email_Rank.PROJECT_ID = ${1}
-
 drop table #rank_assign
-drop table #rank_tie
+create clustered index iWORK_UNITS on #rank_tie_today(WORK_UNITS)
 go
-print ' Rank all, overall'
 
+print ' Create rank table for overall'
 create table #rank_assign
 (
 	IDENT numeric(10, 0) identity,
@@ -78,28 +66,30 @@ insert #rank_assign (ID, WORK_UNITS)
 	where PROJECT_ID = ${1}
 	order by WORK_TOTAL desc, ID desc
 
-create clustered index iWORK_UNITS on #rank_assign(WORK_UNITS)
-
-create table #rank_tie
+create table #rank_tie_overall
 (
 	WORK_UNITS numeric(20, 0),
 	rank int
 )
 go
-insert #rank_tie
+insert #rank_tie_overall
 	select WORK_UNITS, min(IDENT)
 	from #rank_assign
 	group by WORK_UNITS
+go
 
+drop table #rank_assign
+create clustered index iWORK_UNITS on #rank_tie_overall(WORK_UNITS)
+go
+
+print ' Update Email_Rank with new rankings'
 update Email_Rank
-	set OVERALL_RANK = #rank_tie.rank
-	from #rank_tie, #rank_assign
-	where #rank_tie.WORK_UNITS = #rank_assign.WORK_UNITS
-		and Email_Rank.ID = #rank_assign.ID
+	set OVERALL_RANK = o.rank, DAY_RANK = isnull(d.rank, Email_Rank.DAY_RANK)
+	from #rank_tie_overall o, #rank_tie_today d
+	where Email_Rank.WORK_TODAY *= d.WORK_UNITS
+		and Email_Rank.WORK_TOTAL = o.WORK_UNITS
 		and Email_Rank.PROJECT_ID = ${1}
 go
-drop table #rank_assign
-drop table #rank_tie
 
 print ' set previous rank = current rank for new participants'
 go
