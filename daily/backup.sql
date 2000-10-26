627e724504f9027f1f506dfb8be59a78d0ac9e3a
decibel <decibel>
2000-10-26 02:09:58 +0000

    Add some go's for timestamping.
    See how a single delete goes on an un-indexed Team_Members_Backup

--- daily/backup.sql
+++ daily/backup.sql
@@ -1,6 +1,6 @@
 #!/usr/bin/sqsh -i
 #
-# $Id: backup.sql,v 1.7 2000/10/24 00:53:22 decibel Exp $
+# $Id: backup.sql,v 1.8 2000/10/26 02:09:58 decibel Exp $
 #
 # Makes backup copies of Email_Rank, Team_Rank, and Team_Members
 # Arguments:
@@ -16,14 +16,14 @@ delete statproc.Email_Rank_Backup
 	where PROJECT_ID = ${1} and BACKUP_DATE = @stats_date
 delete statproc.Email_Rank_Backup
 	where PROJECT_ID = ${1} and BACKUP_DATE < dateadd(dd, -7, @stats_date)
+go
 delete statproc.Team_Rank_Backup
 	where PROJECT_ID = ${1} and BACKUP_DATE = @stats_date
 delete statproc.Team_Rank_Backup
 	where PROJECT_ID = ${1} and BACKUP_DATE < dateadd(dd, -7, @stats_date)
+go
 delete statproc.Team_Members_Backup
-	where PROJECT_ID = ${1} and BACKUP_DATE = @stats_date
-delete statproc.Team_Members_Backup
-	where PROJECT_ID = ${1} and BACKUP_DATE < dateadd(dd, -7, @stats_date)
+	where PROJECT_ID = ${1} and (BACKUP_DATE = @stats_date or BACKUP_DATE < dateadd(dd, -7, @stats_date)
 go
 
 print "Backing up Email_Rank"
