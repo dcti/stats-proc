96eeebbcf8e8d0a028beaa8e50cc850e0a06b0c0
decibel <decibel>
2002-12-11 03:02:13 +0000

    Make em_update actually work again

--- daily/em_update.sql
+++ daily/em_update.sql
@@ -1,7 +1,7 @@
 #!/usr/bin/sqsh -i
 /*
 #
-# $Id: em_update.sql,v 1.5 2002/12/10 23:13:34 decibel Exp $
+# $Id: em_update.sql,v 1.6 2002/12/11 03:02:13 decibel Exp $
 #
 # Updates the info in the Email_Rank table
 #
@@ -20,31 +20,6 @@ go
 --drop index Email_Rank.iOVERALL_RANK
 --go
 
-print ' Insert new participants'
-go
-
-declare @stats_date smalldatetime
-select @stats_date = LAST_HOURLY_DATE
-	from Project_statsrun
-	where PROJECT_ID = ${1}
--- select @stats_date = isnull(@stats_date, getdate())
-
-declare @max_rank int
-select @max_rank = max_rank from #maxrank
-insert Email_Rank (PROJECT_ID, ID, FIRST_DATE, LAST_DATE, WORK_TODAY, WORK_TOTAL, DAY_RANK, DAY_RANK_PREVIOUS,
-		OVERALL_RANK, OVERALL_RANK_PREVIOUS)
-	select distinct ${1}, ect.CREDIT_ID, @stats_date, @stats_date, 0, 0, @max_rank, @max_rank, @max_rank, @max_rank
-	from Email_Contrib_Today ect, STATS_Participant sp
-	where ect.CREDIT_ID = sp.ID
-		and (sp.RETIRE_TO = 0 or sp.RETIRE_DATE > @stats_date)
-		and ect.CREDIT_ID not in (select ID from Email_Rank where PROJECT_ID=${1})
-		and not exists (select *
-					from STATS_Participant_Blocked spb
-					where spb.ID = ect.CREDIT_ID
-						and spb.ID = sp.ID
-				)
-		and ect.PROJECT_ID = ${1}
-
 /*
 ** TODO: Assign earlier date if others are retired into
 ** Should not attempt to do it here.  It should happen one-up during a retire.
@@ -59,11 +34,6 @@ create table #retired_work
 	WORK_TODAY	numeric(20, 0)
 )
 go
-declare @stats_date smalldatetime
-select @stats_date = LAST_HOURLY_DATE
-	from Project_statsrun
-	where PROJECT_ID = ${1}
-
 insert #retired_work
 	select CREDIT_ID, sum(ect.WORK_UNITS)
 	from Email_Contrib_Today ect
@@ -72,9 +42,15 @@ insert #retired_work
 go
 
 print ' Remove or move "today" info '
+go
+
+declare @stats_date smalldatetime
+select @stats_date = LAST_HOURLY_DATE
+	from Project_statsrun
+	where PROJECT_ID = ${1}
 declare @max_rank int
 select @max_rank = count(*)+1 from STATS_Participant
-select @max_rank as max_rank into #maxrank
+
 update Email_Rank
 	set DAY_RANK_PREVIOUS = DAY_RANK,
 		DAY_RANK = @max_rank,
@@ -83,8 +59,25 @@ update Email_Rank
 		WORK_TODAY = 0
 	where Email_Rank.PROJECT_ID = ${1}
 
-print
-print
+print ""
+print ""
+print ' Insert new participants'
+insert Email_Rank (PROJECT_ID, ID, FIRST_DATE, LAST_DATE, WORK_TODAY, WORK_TOTAL, DAY_RANK, DAY_RANK_PREVIOUS,
+		OVERALL_RANK, OVERALL_RANK_PREVIOUS)
+	select distinct ${1}, ect.CREDIT_ID, @stats_date, @stats_date, 0, 0, @max_rank, @max_rank, @max_rank, @max_rank
+	from Email_Contrib_Today ect, STATS_Participant sp
+	where ect.CREDIT_ID = sp.ID
+		and (sp.RETIRE_TO = 0 or sp.RETIRE_DATE > @stats_date)
+		and ect.CREDIT_ID not in (select ID from Email_Rank where PROJECT_ID=${1})
+		and not exists (select *
+					from STATS_Participant_Blocked spb
+					where spb.ID = ect.CREDIT_ID
+						and spb.ID = sp.ID
+				)
+		and ect.PROJECT_ID = ${1}
+
+print ""
+print ""
 print ' Update with new info'
 update Email_Rank
 	set WORK_TODAY = rw.WORK_TODAY,
