068349dbd0d009a54b32b6c5bf48ab00531ff9e3
joel <joel>
2002-12-21 23:32:44 +0000

    Commented out CREATE INDEX lines, indices are added after data is loaded.

--- ogr-verification/create_stubs.sql
+++ ogr-verification/create_stubs.sql
@@ -1,4 +1,4 @@
--- $Id: create_stubs.sql,v 1.1 2002/12/21 22:57:30 joel Exp $ --
+-- $Id: create_stubs.sql,v 1.2 2002/12/21 23:32:44 joel Exp $ --
 
 CREATE TABLE stubs (
 email VARCHAR(64),
@@ -9,6 +9,6 @@ cpu_type SMALLINT,
 version INT);
 
 
-CREATE INDEX stubs_email_idx ON stubs (email);
-CREATE INDEX stubs_nodecount_idx ON stubs (nodecount);
-CREATE INDEX stubs_stubid_idx ON stubs (stub_id);
+--CREATE INDEX stubs_email_idx ON stubs (email);
+--CREATE INDEX stubs_nodecount_idx ON stubs (nodecount);
+--CREATE INDEX stubs_stubid_idx ON stubs (stub_id);
