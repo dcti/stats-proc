20fcf5f4163d00f322226c85e2369bef3eb8d38f
nerf <nerf>
2002-12-21 18:51:39 +0000

    Convert from perl to shell script

--- ogr-verification/daily.sh
+++ ogr-verification/daily.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/perl
-# $Id: daily.sh,v 1.1 2002/12/21 18:39:32 nerf Exp $
+#!/bin/sh
+# $Id: daily.sh,v 1.2 2002/12/21 18:51:39 nerf Exp $
 
 # addlog.sql drops and creates the logdata table each day, and fills it with filtered(filter.pl) data.
-# id_lookup1.sql creates a table containing an id and an email for each participant.
+# id_lookup.sql creates a table containing an id and an email for each participant.
 # movedata.sql moves the valid data from logdata to the table called nodes and creates indecies on nodes.
 # donenodes.sql creates the donenodes table, does not put any data in it.
-# query2.sql is the big query, fills donenodes with data.
+# query3.sql is the big query, fills donenodes with data.
 
 psql -d ogrstats -f addlog.sql
 psql -d ogrstats -f id_lookup.sql
