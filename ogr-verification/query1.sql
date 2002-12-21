6fb7eed906b692088b3b06b542cd294ae62a54a9
nerf <nerf>
2002-12-21 14:46:32 +0000

    old version

--- ogr-verification/query1.sql
+++
@@ -1,6 +0,0 @@
--- $Id: query1.sql,v 1.2 2002/12/20 23:55:45 nerf Exp $ --
-
-select distinct stub_id, nodecount, (select count(distinct email)
-from logdata B
-where B.nodecount = A.nodecount) AS participants
-from logdata A;
--- ogr-verification/query2.sql
+++
@@ -1,7 +0,0 @@
--- $Id: query2.sql,v 1.2 2002/12/20 23:55:45 nerf Exp $ --
-
-insert into donenodes
-select distinct stub_id, nodecount, (select count(distinct p.stats_id)
-from nodes B, id_lookup p
-where p.email = B.email and B.nodecount = A.nodecount) AS participants
-from nodes A;
