1c38d1e570e5383291f51478bfa219fbb443bd1c
nerf <nerf>
2002-12-22 22:48:35 +0000

    only bcp out the fields we need

--- ogr-verification/create_id_import.sql
+++ ogr-verification/create_id_import.sql
@@ -1,28 +1,11 @@
--- $Id: create_id_import.sql,v 1.3 2002/12/22 22:01:29 nerf Exp $ --
+-- $Id: create_id_import.sql,v 1.4 2002/12/22 22:48:35 nerf Exp $ --
 
 DROP TABLE import_id;
 
 CREATE TABLE import_id (
-id INTEGER,
 email VARCHAR (64),
-password CHAR (8),
-listmode SMALLINT,   
-nonprofit SMALLINT,   
-team INTEGER,   
-retire_to INTEGER,   
-friend_a INTEGER,   
-friend_b INTEGER,   
-friend_c INTEGER,   
-friend_d INTEGER,   
-friend_e INTEGER,   
-dem_yob INTEGER,   
-dem_heard SMALLINT,   
-dem_gender CHAR (1),
-dem_motivation SMALLINT,   
-dem_country VARCHAR (8),
-contact_name VARCHAR (50),
-contact_phone VARCHAR (20),
-motto VARCHAR(300),
-retire_date VARCHAR(20));
+id INTEGER,
+retire_to INTEGER
+);
 
-COPY import_id FROM '/home/nerf/stats.out';
+COPY import_id FROM '/home/nerf/id_import.out';
