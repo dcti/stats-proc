d2b9bfc0bfd0e79aba5cbb62d0b7d10e80a8aa56
nerf <nerf>
2002-12-22 22:01:29 +0000

    minor changes to types

--- ogr-verification/create_id_import.sql
+++ ogr-verification/create_id_import.sql
@@ -1,28 +1,28 @@
--- $Id: create_id_import.sql,v 1.2 2002/12/20 23:55:45 nerf Exp $ --
+-- $Id: create_id_import.sql,v 1.3 2002/12/22 22:01:29 nerf Exp $ --
 
 DROP TABLE import_id;
 
 CREATE TABLE import_id (
-id NUMERIC (6),
+id INTEGER,
 email VARCHAR (64),
 password CHAR (8),
 listmode SMALLINT,   
 nonprofit SMALLINT,   
-team INT,   
-retire_to INT,   
-friend_a INT,   
-friend_b INT,   
-friend_c INT,   
-friend_d INT,   
-friend_e INT,   
-dem_yob INT,   
+team INTEGER,   
+retire_to INTEGER,   
+friend_a INTEGER,   
+friend_b INTEGER,   
+friend_c INTEGER,   
+friend_d INTEGER,   
+friend_e INTEGER,   
+dem_yob INTEGER,   
 dem_heard SMALLINT,   
 dem_gender CHAR (1),
 dem_motivation SMALLINT,   
 dem_country VARCHAR (8),
 contact_name VARCHAR (50),
 contact_phone VARCHAR (20),
-motto TEXT,
-retire_date TEXT);
+motto VARCHAR(300),
+retire_date VARCHAR(20));
 
 COPY import_id FROM '/home/nerf/stats.out';
