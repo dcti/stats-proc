d02a5d536e65bf285c49f037916361768fada255
joel <joel>
2002-12-22 21:29:30 +0000

    Remove create_nodes.sql

--- ogr-verification/daily.sh
+++ ogr-verification/daily.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: daily.sh,v 1.4 2002/12/22 21:24:19 joel Exp $
+# $Id: daily.sh,v 1.5 2002/12/22 21:29:30 joel Exp $
 
 # addlog.sql drops and creates the logdata table each day, and fills it with filtered(filter.pl) data.
 # id_lookup.sql creates a table containing an id and an email for each participant.
@@ -7,8 +7,14 @@
 # donestubs.sql creates the donestubs table, does not put any data in it.
 # query3.sql is the big query, fills donestubs with data.
 
-psql -d ogrstats -f addlog.sql
+psql -d ogrstats -f addlog24.sql
 psql -d ogrstats -f id_lookup.sql
 psql -d ogrstats -f movedata.sql
 #psql -d ogrstats -f donestubs.sql
 psql -d ogrstats -f query3.sql
+
+#psql -d ogrstats25 -f addlog25.sql
+#psql -d ogrstats25 -f id_lookup.sql
+#psql -d ogrstats25 -f movedata.sql
+#psql -d ogrstats25 -f donestubs.sql
+#psql -d ogrstats25 -f query3.sql
