93a863c7ec4d9346ee69a3cacc317b700e952da7
nerf <nerf>
2002-12-22 21:33:11 +0000

    converting to id instead of email (and minor formatting)

--- ogr-verification/movedata.sql
+++ ogr-verification/movedata.sql
@@ -1,10 +1,10 @@
--- $Id: movedata.sql,v 1.5 2002/12/22 21:24:19 joel Exp $ --
+-- $Id: movedata.sql,v 1.6 2002/12/22 21:33:11 nerf Exp $ --
 
 INSERT INTO stubs
-SELECT DISTINCT logdata.stub_id, logdata.nodecount, logdata.os_type,
-logdata.cpu_type, logdata.version, id_lookup.id 
-FROM logdata, id_lookup WHERE logdata.email = id_lookup.email;
+SELECT DISTINCT L.stub_id, L.nodecount, L.os_type,
+	L.cpu_type, L.version, I.id 
+FROM logdata L, id_lookup I WHERE L.email = I.email;
 
-CREATE INDEX stubs_email ON stubs(email);
+CREATE INDEX stubs_id ON stubs(id);
 CREATE INDEX stubs_stub_id ON stubs(stub_id);
 CREATE INDEX stubs_nodecount ON stubs(nodecount);
