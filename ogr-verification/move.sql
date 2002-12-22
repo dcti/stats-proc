68bef0e0a99dedf4a558c37abaeee471222d6290
joel <joel>
2002-12-22 22:41:05 +0000

    stubs table field is called stats_id, not id.

--- ogr-verification/move.sql
+++ ogr-verification/move.sql
@@ -1,5 +1,5 @@
--- $Id: move.sql,v 1.4 2002/12/22 22:14:51 nerf Exp $ --
+-- $Id: move.sql,v 1.5 2002/12/22 22:41:05 joel Exp $ --
 
 INSERT INTO stubs
-SELECT DISTINCT id, stub_id, nodecount, os_type, cpu_type, version
+SELECT DISTINCT stats_id, stub_id, nodecount, os_type, cpu_type, version
 FROM logdata;
--- ogr-verification/movedata.sql
+++ ogr-verification/movedata.sql
@@ -1,10 +1,10 @@
--- $Id: movedata.sql,v 1.6 2002/12/22 21:33:11 nerf Exp $ --
+-- $Id: movedata.sql,v 1.7 2002/12/22 22:41:59 joel Exp $ --
 
 INSERT INTO stubs
 SELECT DISTINCT L.stub_id, L.nodecount, L.os_type,
-	L.cpu_type, L.version, I.id 
+	L.cpu_type, L.version, I.stats_id 
 FROM logdata L, id_lookup I WHERE L.email = I.email;
 
-CREATE INDEX stubs_id ON stubs(id);
+CREATE INDEX stubs_id ON stubs(stats_id);
 CREATE INDEX stubs_stub_id ON stubs(stub_id);
 CREATE INDEX stubs_nodecount ON stubs(nodecount);
