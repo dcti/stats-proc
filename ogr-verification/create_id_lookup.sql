29dfdbae6b9b3ff0dac0fc32278d74d6bcf2e3c7
joel <joel>
2002-12-22 22:39:29 +0000

    Change from varchar(255) to varchar(64) for email field.

--- ogr-verification/create_id_lookup.sql
+++ ogr-verification/create_id_lookup.sql
@@ -1,9 +1,9 @@
--- $Id: create_id_lookup.sql,v 1.5 2002/12/22 22:14:02 nerf Exp $ --
+-- $Id: create_id_lookup.sql,v 1.6 2002/12/22 22:39:29 joel Exp $ --
 
 DROP TABLE id_lookup;
 
 CREATE TABLE id_lookup (
-email VARCHAR (255),
+email VARCHAR (64),
 id INTEGER
 stats_id INTEGER);
 
