dabe39157480cf5d8d8c1cbec4eb5a850e4fe7b9
nerf <nerf>
2002-12-22 21:26:41 +0000

    converting to id instead of email

--- ogr-verification/query3.sql
+++ ogr-verification/query3.sql
@@ -1,7 +1,7 @@
--- $Id: query3.sql,v 1.3 2002/12/21 21:13:22 joel Exp $ --
+-- $Id: query3.sql,v 1.4 2002/12/22 21:26:41 nerf Exp $ --
 
 SELECT DISTINCT stub_id, nodecount, (SELECT count(DISTINCT p.stats_id)
 FROM stubs B, id_lookup p
-WHERE p.email = B.email AND B.nodecount = A.nodecount) AS participants
+WHERE p.id = B.id AND B.nodecount = A.nodecount) AS participants
 INTO donestubs
 FROM stubs A;
