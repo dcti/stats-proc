ab6c3a373da407485397d1f72550018d58405ee4
nerf <nerf>
2002-12-23 13:39:53 +0000

    reducing the number of files

--- ogr-verification/create_id_lookup.sql
+++ ogr-verification/create_id_lookup.sql
@@ -1,4 +1,14 @@
--- $Id: create_id_lookup.sql,v 1.6 2002/12/22 22:39:29 joel Exp $ --
+-- $Id: create_id_lookup.sql,v 1.7 2002/12/23 13:39:53 nerf Exp $ --
+
+DROP TABLE import_id;
+
+CREATE TABLE import_id (
+email VARCHAR (64),
+id INTEGER,
+retire_to INTEGER
+);
+
+COPY import_id FROM '/home/nerf/id_import.out';
 
 DROP TABLE id_lookup;
 
@@ -8,7 +18,8 @@ id INTEGER
 stats_id INTEGER);
 
 INSERT INTO id_lookup
-SELECT email,
-	id,
-	(retire_to * (sign(retire_to)) + id * (1-sign(retire_to))) AS stats_id
-FROM import_id ;
+	SELECT email,
+		id,
+		(retire_to*(sign(retire_to))+id*(1-sign(retire_to)))
+			AS stats_id
+	FROM import_id;
