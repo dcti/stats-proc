ccec4cbcbf8d79ecc7e0973f3da72637f4f41222
bwilson <bwilson>
2002-12-23 00:05:03 +0000

    Completed the join between A and B.  Without this join, rows could be updated if their nodecounts match, even if their stub_id's didn't.  This scenario is unlikely, but should let it run more efficiently.

--- ogr-verification/query3.sql
+++ ogr-verification/query3.sql
@@ -1,7 +1,7 @@
--- $Id: query3.sql,v 1.4 2002/12/22 21:26:41 nerf Exp $ --
+-- $Id: query3.sql,v 1.5 2002/12/23 00:05:03 bwilson Exp $ --
 
 SELECT DISTINCT stub_id, nodecount, (SELECT count(DISTINCT p.stats_id)
 FROM stubs B, id_lookup p
-WHERE p.id = B.id AND B.nodecount = A.nodecount) AS participants
+WHERE p.id = B.id AND B.nodecount = A.nodecount AND B.stub_id = A.stub_id) AS participants
 INTO donestubs
 FROM stubs A;
