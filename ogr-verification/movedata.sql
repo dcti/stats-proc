00cda390f21f3dccc91a01e7b1c1f67f2ebd47b7
joel <joel>
2002-12-23 00:32:59 +0000

    Changed to use new id field.

--- ogr-verification/movedata.sql
+++ ogr-verification/movedata.sql
@@ -1,8 +1,8 @@
--- $Id: movedata.sql,v 1.7 2002/12/22 22:41:59 joel Exp $ --
+-- $Id: movedata.sql,v 1.8 2002/12/23 00:32:59 joel Exp $ --
 
 INSERT INTO stubs
 SELECT DISTINCT L.stub_id, L.nodecount, L.os_type,
-	L.cpu_type, L.version, I.stats_id 
+	L.cpu_type, L.version, I.id 
 FROM logdata L, id_lookup I WHERE L.email = I.email;
 
 CREATE INDEX stubs_id ON stubs(stats_id);
