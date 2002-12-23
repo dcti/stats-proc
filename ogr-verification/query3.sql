412d6e1c31dfc77eda0eacd56efa556e92eddc1f
bwilson <bwilson>
2002-12-23 16:57:28 +0000

    Previous commit had both old and new forms of the subquery query.

--- ogr-verification/query3.sql
+++ ogr-verification/query3.sql
@@ -1,13 +1,4 @@
--- $Id: query3.sql,v 1.7 2002/12/23 16:54:31 bwilson Exp $ --
-
--- id_lookup p became irrelevant as id was already present in B
-SELECT DISTINCT stub_id, nodecount, (SELECT count(DISTINCT B.id)
-FROM stubs B
-WHERE B.nodecount = A.nodecount AND B.stub_id = A.stub_id) AS participants
-INTO donestubs
-FROM stubs A;
-
--- Or better yet...
+-- $Id: query3.sql,v 1.8 2002/12/23 16:57:28 bwilson Exp $ --
 
 create table donestubs
 (
