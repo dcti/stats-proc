86a200b7828ff9502ffec1b080fb6e303d2c5ec7
joel <joel>
2002-12-23 02:39:51 +0000

    Wrong, it is id.

--- ogr-verification/move.sql
+++ ogr-verification/move.sql
@@ -1,5 +1,5 @@
--- $Id: move.sql,v 1.5 2002/12/22 22:41:05 joel Exp $ --
+-- $Id: move.sql,v 1.6 2002/12/23 02:39:51 joel Exp $ --
 
 INSERT INTO stubs
-SELECT DISTINCT stats_id, stub_id, nodecount, os_type, cpu_type, version
+SELECT DISTINCT id, stub_id, nodecount, os_type, cpu_type, version
 FROM logdata;
