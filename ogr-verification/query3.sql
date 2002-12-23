2b1d8c454ea0c8ac42422bcbe76d21d597473608
bwilson <bwilson>
2002-12-23 16:54:31 +0000

    Rewrote select into/subquery in query3 as a create table/insert without subquery

--- ogr-verification/query3.sql
+++ ogr-verification/query3.sql
@@ -1,7 +1,22 @@
--- $Id: query3.sql,v 1.6 2002/12/23 01:35:30 joel Exp $ --
+-- $Id: query3.sql,v 1.7 2002/12/23 16:54:31 bwilson Exp $ --
 
-SELECT DISTINCT stub_id, nodecount, (SELECT count(DISTINCT p.id)
-FROM stubs B, id_lookup p
-WHERE p.id = B.id AND B.nodecount = A.nodecount AND B.stub_id = A.stub_id) AS participants
+-- id_lookup p became irrelevant as id was already present in B
+SELECT DISTINCT stub_id, nodecount, (SELECT count(DISTINCT B.id)
+FROM stubs B
+WHERE B.nodecount = A.nodecount AND B.stub_id = A.stub_id) AS participants
 INTO donestubs
 FROM stubs A;
+
+-- Or better yet...
+
+create table donestubs
+(
+	stub_id varchar(22),
+	nodecount bigint,
+	participants int
+)
+
+insert donestubs
+	select stub_id, nodecount, count(distinct B.id)
+	from stubs B
+
