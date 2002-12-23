c160df974936e30fe510e21168a2c0ec5095d7c2
joel <joel>
2002-12-23 00:30:37 +0000

    Added id field.

--- ogr-verification/fake_idlookup.sql
+++ ogr-verification/fake_idlookup.sql
@@ -1,19 +1,19 @@
--- $Id: fake_idlookup.sql,v 1.2 2002/12/22 23:24:51 joel Exp $
+-- $Id: fake_idlookup.sql,v 1.3 2002/12/23 00:30:37 joel Exp $
 
 DROP TABLE id_lookup;
-DROP SEQUENCE id_lookup_stats_id_seq;
-CREATE SEQUENCE id_lookup_stats_id_seq START 1;
+DROP SEQUENCE id;
 DROP SEQUENCE id_lookup_id_seq;
-CREATE SEQUENCE id_lookup_id_seq START 1;
+CREATE SEQUENCE id START 1;
 
 CREATE TABLE id_lookup (
-stats_id BIGSERIAL,
 id BIGSERIAL,
-email VARCHAR(64));
+stats_id INT,
+email VARCHAR(64),
+PRIMARY KEY (id));
 
-INSERT INTO id_lookup
-SELECT nextval('stats_id'), nextval('id'), DISTINCT email
-FROM logdata
+INSERT INTO id_lookup 
+SELECT nextval('id'), nextval('id'), email 
+FROM logdata 
 GROUP BY email;
 
-CREATE INDEX id_lookup_email_idx ON id_lookup (email);
+CREATE INDEX idlookup_email_idx ON id_lookup (email);
