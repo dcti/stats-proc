818784ccadfd33ad1b58881b2113e21ce29186b3
joel <joel>
2002-12-23 01:53:12 +0000

    Change from email to id.

--- ogr-verification/addlog24.sql
+++ ogr-verification/addlog24.sql
@@ -1,4 +1,4 @@
--- $Id: addlog24.sql,v 1.1 2002/12/21 21:13:22 joel Exp $ --
+-- $Id: addlog24.sql,v 1.2 2002/12/23 01:53:12 joel Exp $ --
 
 DROP TABLE logdata;
 
@@ -8,11 +8,12 @@ stub_id VARCHAR(22),
 nodecount BIGINT,
 os_type SMALLINT,
 cpu_type SMALLINT,
-version INT);
+version INT,
+id INT);
 
 
 COPY logdata FROM '/home/joel/ogr/scripts/ogr24.filtered' USING DELIMITERS ',';
 
-CREATE INDEX email_idx ON logdata (email);
+CREATE INDEX id_idx ON logdata (id);
 CREATE INDEX nodecount_idx ON logdata (nodecount);
 CREATE INDEX stubid_idx ON logdata (stub_id);
