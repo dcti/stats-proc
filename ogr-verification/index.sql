2e43221ba661c0515f1c873aa20e8cd305ac1519
nerf <nerf>
2002-12-24 22:32:04 +0000

    adding indices

--- ogr-verification/index.sql
+++ ogr-verification/index.sql
@@ -1,8 +1,15 @@
--- $Id: index.sql,v 1.4 2002/12/22 22:14:34 nerf Exp $ --
+-- $Id: index.sql,v 1.5 2002/12/24 22:32:04 nerf Exp $ --
+
+-- All the indices
 
 CREATE INDEX stubs_id ON stubs(id);
 CREATE INDEX stubs_stub_id ON stubs(stub_id);
 CREATE INDEX stubs_nodecount ON stubs(nodecount);
-CREATE INDEX stubs_os_type ON stubs(os_type);
-CREATE INDEX stubs_cpu_type ON stubs(cpu_type);
-CREATE INDEX stubs_version ON stubs(version);
+CREATE INDEX all_stubs_marks ON all_stubs(stub_marks);
+ALTER TABLE all_stubs ADD PRIMARY KEY (stub_id);
+CREATE INDEX done_participants ON donenodes(participants);
+ALTER TABLE id_lookup ADD PRIMARY KEY (id);
+CREATE INDEX idlookup_email_idx ON id_lookup (email);
+CREATE INDEX log_email_idx ON logdata (email);
+CREATE INDEX log_nodecount_idx ON logdata (nodecount);
+CREATE INDEX log_stubmark_idx ON logdata (stub_marks);
