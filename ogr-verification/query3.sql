57c91fadcad3b60efcbd812eea53988ec74bbade
nerf <nerf>
2002-12-25 01:05:35 +0000

    A couple of grammer changes. now uses id instead of email

--- ogr-verification/query3.sql
+++ ogr-verification/query3.sql
@@ -1,14 +1,14 @@
--- $Id: query3.sql,v 1.10 2002/12/24 00:05:32 bwilson Exp $ --
+-- $Id: query3.sql,v 1.11 2002/12/25 01:05:35 nerf Exp $ --
 
 create table donestubs
 (
 	stub_id int,
 	nodecount bigint,
 	participants int
-)
+);
 
-insert donestubs
+insert into donestubs
 	select stub_id, nodecount, count(distinct p.stats_id)
-	from nodes B, id_lookup p
-	where p.email = B.email
-	group by stub_id, nodecount
+	from stubs B, id_lookup p
+	where p.id = B.id
+	group by stub_id, nodecount;
