da7df1ad42bd5c9a909bd8598c73b3f59e48021b
nerf <nerf>
2002-12-25 01:04:20 +0000

    id can't be a primary index unless it's 'not null' - and I'm not sure we want that

--- ogr-verification/index.sql
+++ ogr-verification/index.sql
@@ -1,4 +1,4 @@
--- $Id: index.sql,v 1.5 2002/12/24 22:32:04 nerf Exp $ --
+-- $Id: index.sql,v 1.6 2002/12/25 01:04:20 nerf Exp $ --
 
 -- All the indices
 
@@ -8,7 +8,7 @@ CREATE INDEX stubs_nodecount ON stubs(nodecount);
 CREATE INDEX all_stubs_marks ON all_stubs(stub_marks);
 ALTER TABLE all_stubs ADD PRIMARY KEY (stub_id);
 CREATE INDEX done_participants ON donenodes(participants);
-ALTER TABLE id_lookup ADD PRIMARY KEY (id);
+CREATE INDEX idlookup_id ON id_lookup (id);
 CREATE INDEX idlookup_email_idx ON id_lookup (email);
 CREATE INDEX log_email_idx ON logdata (email);
 CREATE INDEX log_nodecount_idx ON logdata (nodecount);
