9b3d35c84af783a2b03d2e6115b8f758450d15b5
nerf <nerf>
2002-12-26 00:39:01 +0000

    Fields were in wrong order

--- ogr-verification/movedata.sql
+++ ogr-verification/movedata.sql
@@ -1,8 +1,8 @@
--- $Id: movedata.sql,v 1.10 2002/12/23 18:44:06 nerf Exp $ --
+-- $Id: movedata.sql,v 1.10.2.1 2002/12/26 00:39:01 nerf Exp $ --
 
 INSERT INTO stubs
-SELECT DISTINCT A.stub_id, L.nodecount, L.os_type,
-	L.cpu_type, L.version, I.id 
+SELECT DISTINCT I.id, A.stub_id, L.nodecount, L.os_type,
+	L.cpu_type, L.version
 FROM logdata L, id_lookup I, all_stubs A
 WHERE L.email = I.email AND
 	L.stub_marks = A.stub_marks;
