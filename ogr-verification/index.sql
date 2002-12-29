ab1c425a3ba753f83d90f711ec8012d33734cbfa
nerf <nerf>
2002-12-29 22:32:55 +0000

    changed reference to nodes to stubs

--- ogr-verification/index.sql
+++ ogr-verification/index.sql
@@ -1,4 +1,4 @@
--- $Id: index.sql,v 1.6 2002/12/25 01:04:20 nerf Exp $ --
+-- $Id: index.sql,v 1.7 2002/12/29 22:32:55 nerf Exp $ --
 
 -- All the indices
 
@@ -7,7 +7,7 @@ CREATE INDEX stubs_stub_id ON stubs(stub_id);
 CREATE INDEX stubs_nodecount ON stubs(nodecount);
 CREATE INDEX all_stubs_marks ON all_stubs(stub_marks);
 ALTER TABLE all_stubs ADD PRIMARY KEY (stub_id);
-CREATE INDEX done_participants ON donenodes(participants);
+CREATE INDEX done_participants ON donestubs(participants);
 CREATE INDEX idlookup_id ON id_lookup (id);
 CREATE INDEX idlookup_email_idx ON id_lookup (email);
 CREATE INDEX log_email_idx ON logdata (email);
