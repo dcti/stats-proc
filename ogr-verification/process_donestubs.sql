174211f61debb26fff555099f354854668d477c1
nerf <nerf>
2002-12-30 08:21:11 +0000

    No need to return dupe stubs

--- ogr-verification/process_donestubs.sql
+++ ogr-verification/process_donestubs.sql
@@ -1,8 +1,8 @@
--- $Id: process_donestubs.sql,v 1.2 2002/12/30 06:43:38 nerf Exp $
+-- $Id: process_donestubs.sql,v 1.3 2002/12/30 08:21:11 nerf Exp $
 
-select A.stub_marks
-from donestubs D, all_stubs A, stubs S
-where D.stub_id = A.stub_id AND
+SELECT DISTINCT A.stub_marks
+FROM donestubs D, all_stubs A, stubs S
+WHERE D.stub_id = A.stub_id AND
 	D.stub_id = S.stub_id AND
 	D.nodecount = S.nodecount AND
 	D.participants >= 2 AND
