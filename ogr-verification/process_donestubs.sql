6aee9a7df0be44ca06e68eaf456f41b0f2445f0b
nerf <nerf>
2002-12-30 06:43:38 +0000

    Fixing typo and reducing fields returned

--- ogr-verification/process_donestubs.sql
+++ ogr-verification/process_donestubs.sql
@@ -1,10 +1,10 @@
--- $Id: process_donestubs.sql,v 1.1 2002/12/29 00:30:00 nerf Exp $
+-- $Id: process_donestubs.sql,v 1.2 2002/12/30 06:43:38 nerf Exp $
 
-select A.*, D.*
+select A.stub_marks
 from donestubs D, all_stubs A, stubs S
 where D.stub_id = A.stub_id AND
 	D.stub_id = S.stub_id AND
 	D.nodecount = S.nodecount AND
-	D.participant >= 2 AND
+	D.participants >= 2 AND
 	S.version > 8014;
 
