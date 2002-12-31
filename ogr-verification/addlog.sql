190b43e22457e53797affb1896b1111455447608
joel <joel>
2002-12-31 18:08:45 +0000

    Fix index creation, again.

--- ogr-verification/addlog.sql
+++ ogr-verification/addlog.sql
@@ -1,4 +1,4 @@
--- $Id: addlog.sql,v 1.5 2002/12/31 16:46:56 joel Exp $ --
+-- $Id: addlog.sql,v 1.6 2002/12/31 18:08:45 joel Exp $ --
 --set projnum '24' or '25'
 --set infile '/home/joel/ogr/scripts/ogr24.filtered'
 
@@ -15,6 +15,6 @@ version INT);
 
 COPY logdata:projnum FROM :infile USING DELIMITERS ',';
 
-CREATE INDEX log:projnum_email_id ON log:projnum (email);
-CREATE INDEX log:projnum_nodecount_idx ON log:projnum (nodecount);
-CREATE INDEX log:projnum_stubmark_idx ON log:projnum (stub_marks);
+CREATE INDEX log_email_idx:projnum ON logdata:projnum (email);
+CREATE INDEX log_nodecount_idx:projnum ON logdata:projnum (nodecount);
+CREATE INDEX log_stubmark_idx:projnum ON logdata:projnum (stub_marks);
