fb5fb54c1ef5c946416c0adcaef4af73f1dba328
nerf <nerf>
2003-01-04 21:42:37 +0000

    Make all_stubs have room to hold info for the two passes

--- ogr-verification/create_all_stubs.sql
+++ ogr-verification/create_all_stubs.sql
@@ -1,8 +1,5 @@
--- $Id: create_all_stubs.sql,v 1.2 2003/01/01 17:01:05 joel Exp $
+-- $Id: create_all_stubs.sql,v 1.3 2003/01/04 21:42:37 nerf Exp $
 -- set infile='/home/nerf/all_stubs'
-
---DROP SEQUENCE stub_id;
-CREATE SEQUENCE stub_id START 1;
 
 CREATE TEMPORARY TABLE all_stubs_import:projnum (
 stub_marks VARCHAR(22) not null);
@@ -10,9 +7,12 @@ stub_marks VARCHAR(22) not null);
 COPY all_stubs_import:projnum FROM :infile;
 
 CREATE TABLE all_stubs:projnum (
-stub_marks VARCHAR(22) not null,
-stub_id INT DEFAULT NEXTVAL('stub_id') not null,
-completed INT DEFAULT 0);
+	stub_marks VARCHAR(22) not null,
+	stub_id SERIAL,
+	nodecount BIGINT,
+	pass1_id INT,
+	pass2_id INT
+);
 
 INSERT INTO all_stubs:projnum
         SELECT stub_marks, NEXTVAL('stub_id'), 0
