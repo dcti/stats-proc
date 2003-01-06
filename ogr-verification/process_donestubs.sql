288424bf7a732dd61946e1a6cc4312861f3c32d6
nerf <nerf>
2003-01-06 08:06:58 +0000

    change > to >=

--- ogr-verification/process_donestubs.sql
+++ ogr-verification/process_donestubs.sql
@@ -1,4 +1,4 @@
--- $Id: process_donestubs.sql,v 1.3 2002/12/30 08:21:11 nerf Exp $
+-- $Id: process_donestubs.sql,v 1.4 2003/01/06 08:06:58 nerf Exp $
 
 SELECT DISTINCT A.stub_marks
 FROM donestubs D, all_stubs A, stubs S
@@ -6,5 +6,4 @@ WHERE D.stub_id = A.stub_id AND
 	D.stub_id = S.stub_id AND
 	D.nodecount = S.nodecount AND
 	D.participants >= 2 AND
-	S.version > 8014;
-
+	S.version >= 8014;
