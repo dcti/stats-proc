205afa9868529f4a47e9dc82b3efeb5aafd959d4
joel <joel>
2003-01-08 04:18:09 +0000

    Fix duplicate entry.

--- ogr-verification/initial.sh
+++ ogr-verification/initial.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-# $Id: initial.sh,v 1.5 2003/01/08 02:25:34 joel Exp $
+# $Id: initial.sh,v 1.6 2003/01/08 04:18:09 joel Exp $
 #
-psql -d ogrstats -f create_stubs.sql 
 psql -d ogrstats -f create_stubs.sql 
