5f977e6846e755c03d5045d7964276d3464c6b44
nerf <nerf>
2003-01-10 01:31:37 +0000

    fixing typo

--- ogr-verification/movedata.sql
+++ ogr-verification/movedata.sql
@@ -1,9 +1,9 @@
--- $Id: movedata.sql,v 1.13 2003/01/08 02:27:19 joel Exp $ --
+-- $Id: movedata.sql,v 1.14 2003/01/10 01:31:37 nerf Exp $ --
 
 INSERT INTO stubs
 SELECT DISTINCT I.id, A.stub_id, L.nodecount, L.os_type,
 	L.cpu_type, L.version
-FROM logdataL, id_lookup I, all_stubsA
+FROM logdata L, id_lookup I, all_stubs A
 WHERE L.email = I.email AND
 	L.stub_marks = A.stub_marks;
 
