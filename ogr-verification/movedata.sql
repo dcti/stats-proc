f1e5d55837e4b2ad4b7fa5cd8f9f306beb13c4ed
nerf <nerf>
2003-01-14 22:12:15 +0000

    Put inside one commit block

--- ogr-verification/movedata.sql
+++ ogr-verification/movedata.sql
@@ -1,11 +1,13 @@
--- $Id: movedata.sql,v 1.14 2003/01/10 01:31:37 nerf Exp $ --
+-- $Id: movedata.sql,v 1.15 2003/01/14 22:12:15 nerf Exp $ --
 
-INSERT INTO stubs
-SELECT DISTINCT I.id, A.stub_id, L.nodecount, L.os_type,
-	L.cpu_type, L.version
-FROM logdata L, id_lookup I, all_stubs A
-WHERE L.email = I.email AND
-	L.stub_marks = A.stub_marks;
+BEGIN;
+	INSERT INTO stubs
+	SELECT DISTINCT I.id, A.stub_id, L.nodecount, L.os_type,
+		L.cpu_type, L.version
+	FROM logdata L, id_lookup I, all_stubs A
+	WHERE L.email = I.email AND
+		L.stub_marks = A.stub_marks;
+COMMIT;
 
 --CREATE INDEX stubs_id ON stubs(id);
 --CREATE INDEX stubs_stub_id ON stubs(stub_id);
