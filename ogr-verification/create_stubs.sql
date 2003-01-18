a7f542e00ed8a7f3d8c415cac03348374128d3d7
nerf <nerf>
2003-01-18 00:53:50 +0000

    Added field return_count to stubs that holds number of times that id,stub,etc have been returned

--- ogr-verification/create_stubs.sql
+++ ogr-verification/create_stubs.sql
@@ -1,4 +1,4 @@
--- $Id: create_stubs.sql,v 1.7 2003/01/08 02:20:44 joel Exp $ --
+-- $Id: create_stubs.sql,v 1.8 2003/01/18 00:53:50 nerf Exp $ --
 
 CREATE TABLE stubs (
 id INT,
@@ -6,8 +6,10 @@ stub_id INTEGER,
 nodecount BIGINT,
 os_type SMALLINT,
 cpu_type SMALLINT,
-version INT);
+version INTEGER,
+return_count INTEGER
+);
 
-CREATE INDEX stubs_email_idx ON stubs (email);
-CREATE INDEX stubs_nodecount_idx ON stubs (nodecount);
-CREATE INDEX stubs_stubid_idx ON stubs (stub_id);
+--CREATE INDEX stubs_email ON stubs (email);
+--CREATE INDEX stubs_nodecount ON stubs (nodecount);
+--CREATE INDEX stubs_stubid ON stubs (stub_id);
--- ogr-verification/movedata.sql
+++ ogr-verification/movedata.sql
@@ -1,12 +1,44 @@
--- $Id: movedata.sql,v 1.15 2003/01/14 22:12:15 nerf Exp $ --
+-- $Id: movedata.sql,v 1.16 2003/01/18 00:53:50 nerf Exp $ --
+
+CREATE TABLE TEMP normal_stubs (
+	id INT,
+	stub_id INTEGER,
+	nodecount BIGINT,
+	os_type SMALLINT,
+	cpu_type SMALLINT,
+	version INT);
 
 BEGIN;
-	INSERT INTO stubs
-	SELECT DISTINCT I.id, A.stub_id, L.nodecount, L.os_type,
+	INSERT INTO normal_stubs
+	SELECT I.id, A.stub_id, L.nodecount, L.os_type,
 		L.cpu_type, L.version
 	FROM logdata L, id_lookup I, all_stubs A
 	WHERE L.email = I.email AND
 		L.stub_marks = A.stub_marks;
+
+	UPDATE stubs s 
+		SET s.return_count = s.return_count +
+			( SELECT count(*)
+				FROM normal_stubs n
+				WHERE n.id = s.id AND
+					n.stub_id = s.stub_id AND 
+					n.nodecount = s.nodecount AND
+					n.os_type = s.os_type AND
+					n.cpu_type = s.cpu_type AND
+					n.version = s.version)
+
+	INSERT INTO stubs s
+	SELECT n.*
+	FROM normal_stubs n
+	WHERE NOT EXISTS (SELECT 1 WHERE n.id = s.id AND
+				n.stub_id = s.stub_id AND
+				n.nodecount = s.nodecount AND
+				n.os_type = s.os_type AND
+				n.cpu_type = s.cpu_type AND
+				n.version = s.version)
+	GROUP BY n.id, n.stub_id, n.nodecount, n.os_type, n.cpu_type, n.version;
+
+	DROP TABLE logdata;
 COMMIT;
 
 --CREATE INDEX stubs_id ON stubs(id);
