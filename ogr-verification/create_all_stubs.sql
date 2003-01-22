44a0227ac4917ec3984776c8631453236f737cd6
nerf <nerf>
2003-01-22 01:35:39 +0000

    remove the multi-pass fields

--- ogr-verification/create_all_stubs.sql
+++ ogr-verification/create_all_stubs.sql
@@ -1,4 +1,4 @@
--- $Id: create_all_stubs.sql,v 1.7 2003/01/10 22:01:37 nerf Exp $
+-- $Id: create_all_stubs.sql,v 1.8 2003/01/22 01:35:39 nerf Exp $
 
 CREATE TEMPORARY TABLE all_stubs_import(
 stub_marks VARCHAR(22) not null);
@@ -8,12 +8,9 @@ COPY all_stubs_import FROM '/home/nerf/all_stubs24';
 DROP SEQUENCE all_stubs_stub_id_seq;
 
 CREATE TABLE all_stubs (
-	stub_marks VARCHAR(22) not null,
+	stub_marks VARCHAR(22) NOT NULL,
 	stub_id UNIQUE SERIAL,
-	project_id SMALLINT not null,
-	nodecount BIGINT,
-	pass1_id INT,
-	pass2_id INT
+	project_id SMALLINT NOT NULL
 );
 
 INSERT INTO all_stubs
