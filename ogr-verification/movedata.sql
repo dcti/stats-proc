7dbe1b0963f57b8e40b962ab81a1364d64dc9483
nerf <nerf>
2003-01-22 18:36:15 +0000

    rename temp database to make it more clear what its function is

--- ogr-verification/movedata.sql
+++ ogr-verification/movedata.sql
@@ -1,6 +1,6 @@
--- $Id: movedata.sql,v 1.19 2003/01/22 17:01:50 nerf Exp $ --
+-- $Id: movedata.sql,v 1.20 2003/01/22 18:36:15 nerf Exp $ --
 
-CREATE TEMP TABLE normal_stubs (
+CREATE TEMP TABLE day_results (
 	id INT,
 	stub_id INTEGER,
 	nodecount BIGINT,
@@ -9,45 +9,45 @@ CREATE TEMP TABLE normal_stubs (
 	version INT) ;
 
 BEGIN;
-	INSERT INTO normal_stubs
+	INSERT INTO day_results
 	SELECT I.id, A.stub_id, L.nodecount, L.os_type,
 		L.cpu_type, L.version
 	FROM logdata L, id_lookup I, all_stubs A
 	WHERE lower(L.email) = lower(I.email) AND
 		L.stub_marks = A.stub_marks;
 
-	CREATE INDEX normal_all ON normal_stubs
+	CREATE INDEX dayresults_all ON day_results
 		(id,stub_id,nodecount,os_type,cpu_type,version); 
 
 	UPDATE stubs
-	SET return_count = return_count +
+	SET return_count = COALESCE(return_count,0) +
 		( SELECT count(*)
-		FROM normal_stubs n
-		WHERE n.id = stubs.id AND
-			n.stub_id = stubs.stub_id AND
-			n.nodecount = stubs.nodecount AND
-			n.os_type = stubs.os_type AND
-			n.cpu_type = stubs.cpu_type AND
-			n.version = stubs.version)
+		FROM day_results dr
+		WHERE dr.id = stubs.id AND
+			dr.stub_id = stubs.stub_id AND
+			dr.nodecount = stubs.nodecount AND
+			dr.os_type = stubs.os_type AND
+			dr.cpu_type = stubs.cpu_type AND
+			dr.version = stubs.version)
 	WHERE exists
-	(SELECT * from normal_stubs n WHERE n.id = stubs.id AND
-		n.stub_id = stubs.stub_id AND
-		n.nodecount = stubs.nodecount AND
-		n.os_type = stubs.os_type AND
-		n.cpu_type = stubs.cpu_type AND
-		n.version = stubs.version);
+	(SELECT * from day_results dr WHERE dr.id = stubs.id AND
+		dr.stub_id = stubs.stub_id AND
+		dr.nodecount = stubs.nodecount AND
+		dr.os_type = stubs.os_type AND
+		dr.cpu_type = stubs.cpu_type AND
+		dr.version = stubs.version);
 
 	INSERT INTO stubs
-	SELECT n.id, n.stub_id, n.nodecount, n.os_type, n.cpu_type,
-		n.version 
-	FROM normal_stubs n 
-	WHERE NOT EXISTS (SELECT 1 WHERE n.id = stubs.id AND
-				n.stub_id = stubs.stub_id AND
-				n.nodecount = stubs.nodecount AND
-				n.os_type = stubs.os_type AND
-				n.cpu_type = stubs.cpu_type AND
-				n.version = stubs.version)
-	GROUP BY n.id, n.stub_id, n.nodecount, n.os_type, n.cpu_type, n.version;
+	SELECT dr.id, dr.stub_id, dr.nodecount, dr.os_type, dr.cpu_type,
+		dr.version 
+	FROM day_results dr 
+	WHERE NOT EXISTS (SELECT 1 WHERE dr.id = stubs.id AND
+				dr.stub_id = stubs.stub_id AND
+				dr.nodecount = stubs.nodecount AND
+				dr.os_type = stubs.os_type AND
+				dr.cpu_type = stubs.cpu_type AND
+				dr.version = stubs.version)
+	GROUP BY dr.id, dr.stub_id, dr.nodecount, dr.os_type, dr.cpu_type, dr.version;
 
 	DROP TABLE logdata;
 
