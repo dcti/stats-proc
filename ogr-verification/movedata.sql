ade5338eee1f1f4735eb2b68849f5d2ca7926379
nerf <nerf>
2003-01-22 01:22:21 +0000

    fixed insert

--- ogr-verification/movedata.sql
+++ ogr-verification/movedata.sql
@@ -1,44 +1,46 @@
--- $Id: movedata.sql,v 1.16 2003/01/18 00:53:50 nerf Exp $ --
+-- $Id: movedata.sql,v 1.17 2003/01/22 01:22:21 nerf Exp $ --
 
-CREATE TABLE TEMP normal_stubs (
+CREATE TEMP TABLE normal_stubs (
 	id INT,
 	stub_id INTEGER,
 	nodecount BIGINT,
 	os_type SMALLINT,
 	cpu_type SMALLINT,
-	version INT);
+	version INT) ;
 
 BEGIN;
 	INSERT INTO normal_stubs
 	SELECT I.id, A.stub_id, L.nodecount, L.os_type,
 		L.cpu_type, L.version
 	FROM logdata L, id_lookup I, all_stubs A
-	WHERE L.email = I.email AND
+	WHERE lower(L.email) = lower(I.email) AND
 		L.stub_marks = A.stub_marks;
 
-	UPDATE stubs s 
-		SET s.return_count = s.return_count +
-			( SELECT count(*)
-				FROM normal_stubs n
-				WHERE n.id = s.id AND
-					n.stub_id = s.stub_id AND 
-					n.nodecount = s.nodecount AND
-					n.os_type = s.os_type AND
-					n.cpu_type = s.cpu_type AND
-					n.version = s.version)
+--This update isn't working for some reason
+	UPDATE stubs
+	SET return_count = return_count +
+		( SELECT count(*)
+		FROM normal_stubs n, stubs
+		WHERE n.id = stubs.id AND
+			n.stub_id = stubs.stub_id AND 
+			n.nodecount = stubs.nodecount AND
+			n.os_type = stubs.os_type AND
+			n.cpu_type = stubs.cpu_type AND
+			n.version = stubs.version);
 
-	INSERT INTO stubs s
-	SELECT n.*
-	FROM normal_stubs n
-	WHERE NOT EXISTS (SELECT 1 WHERE n.id = s.id AND
-				n.stub_id = s.stub_id AND
-				n.nodecount = s.nodecount AND
-				n.os_type = s.os_type AND
-				n.cpu_type = s.cpu_type AND
-				n.version = s.version)
+	INSERT INTO stubs
+	SELECT n.id, n.stub_id, n.nodecount, n.os_type, n.cpu_type,
+		n.version --, count(*) as return_count
+	FROM normal_stubs n 
+	WHERE NOT EXISTS (SELECT 1 WHERE n.id = stubs.id AND
+				n.stub_id = stubs.stub_id AND
+				n.nodecount = stubs.nodecount AND
+				n.os_type = stubs.os_type AND
+				n.cpu_type = stubs.cpu_type AND
+				n.version = stubs.version)
 	GROUP BY n.id, n.stub_id, n.nodecount, n.os_type, n.cpu_type, n.version;
 
-	DROP TABLE logdata;
+--	DROP TABLE logdata;
 COMMIT;
 
 --CREATE INDEX stubs_id ON stubs(id);
