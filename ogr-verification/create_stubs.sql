de35e2fc8b280f16149651022ca24c7003d4c2f3
nerf <nerf>
2003-01-22 01:38:37 +0000

    an index on (almost) everything helps greatly for the movedata stuff

--- ogr-verification/create_stubs.sql
+++ ogr-verification/create_stubs.sql
@@ -1,4 +1,4 @@
--- $Id: create_stubs.sql,v 1.8 2003/01/18 00:53:50 nerf Exp $ --
+-- $Id: create_stubs.sql,v 1.9 2003/01/22 01:38:37 nerf Exp $ --
 
 CREATE TABLE stubs (
 id INT,
@@ -13,3 +13,5 @@ return_count INTEGER
 --CREATE INDEX stubs_email ON stubs (email);
 --CREATE INDEX stubs_nodecount ON stubs (nodecount);
 --CREATE INDEX stubs_stubid ON stubs (stub_id);
+CREATE UNIQUE INDEX stubs_all on stubs
+	(id,stub_id,nodecount,os_type,cpu_type,version); 
