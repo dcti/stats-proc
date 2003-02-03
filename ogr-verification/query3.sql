2bf78f57448a16ee0b1a11bb88f5db6e7bba8de9
nerf <nerf>
2003-02-03 05:49:28 +0000

    Truncate the table before we start.

--- ogr-verification/query3.sql
+++ ogr-verification/query3.sql
@@ -1,4 +1,4 @@
--- $Id: query3.sql,v 1.14 2003/01/14 22:11:01 nerf Exp $ --
+-- $Id: query3.sql,v 1.15 2003/02/03 05:49:28 nerf Exp $ --
 
 create table donestubs
 (
@@ -7,8 +7,12 @@ create table donestubs
 	participants int
 );
 
+truncate table donestubs;
+
+select now();
 insert into donestubs
 	select stub_id, nodecount, count(distinct p.stats_id)
 	from stubs B, id_lookup p
 	where p.id = B.id
 	group by stub_id, nodecount;
+select now();
