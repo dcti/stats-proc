4ed76ab0776321c3789af2b51ce6f6120d25b221
nerf <nerf>
2003-02-03 05:48:56 +0000

    Now that platform_id is a lookup, need to have platform in the join to get the client version.

--- ogr-verification/process_donestubs.sql
+++ ogr-verification/process_donestubs.sql
@@ -1,12 +1,13 @@
--- $Id: process_donestubs.sql,v 1.5 2003/01/22 01:24:34 nerf Exp $
+-- $Id: process_donestubs.sql,v 1.6 2003/02/03 05:48:56 nerf Exp $
 
 DROP TABLE confirmed;
 
 CREATE TABLE confirmed AS
 SELECT DISTINCT A.stub_marks
-FROM donestubs D, all_stubs A, stubs S
+FROM donestubs D, all_stubs A, stubs S, platform P
 WHERE D.stub_id = A.stub_id AND
 	D.stub_id = S.stub_id AND
 	D.nodecount = S.nodecount AND
+	S.platform_id = P.platform_id AND
 	D.participants >= 2 AND
-	S.version >= 8014;
+	P.version >= 8014;
