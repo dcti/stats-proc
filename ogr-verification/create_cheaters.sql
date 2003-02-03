1536a1e4b9dbae1adbd957bdb6ce49387022d2f8
nerf <nerf>
2003-02-03 05:35:06 +0000

    Use lower(email)

--- ogr-verification/create_cheaters.sql
+++ ogr-verification/create_cheaters.sql
@@ -1,4 +1,4 @@
--- $Id: create_cheaters.sql,v 1.6 2003/01/08 02:19:43 joel Exp $
+-- $Id: create_cheaters.sql,v 1.7 2003/02/03 05:35:06 nerf Exp $
 -- Create a table where we keep track of how many stubs someone has
 -- returned vs how many unique ones they have returned.  Used to find
 -- people who submit the same stub over and over.
@@ -12,8 +12,8 @@ INSERT INTO cheaters
 	SELECT I.id, count(A.stub_id) AS returned,
 		count(DISTINCT A.stub_id) AS uniq_stubs
 	FROM logdata L, id_lookup I, all_stubs A
-	WHERE L.email = I.email AND
-		L.stub_marks = A.stub_id
+	WHERE lower(L.email) = lower(I.email) AND
+		L.stub_marks = A.stub_marks
 	GROUP BY id;
 
 CREATE new_cheaters AS
