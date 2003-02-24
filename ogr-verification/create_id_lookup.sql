cf0f6ce26420a4924bc1053e147397d3d4c62b70
nerf <nerf>
2003-02-24 22:16:08 +0000

    truncate instead of drop/create and put into a begin/commit block

--- ogr-verification/create_id_lookup.sql
+++ ogr-verification/create_id_lookup.sql
@@ -1,19 +1,23 @@
--- $Id: create_id_lookup.sql,v 1.12 2003/02/16 19:18:42 nerf Exp $ --
+-- $Id: create_id_lookup.sql,v 1.13 2003/02/24 22:16:08 nerf Exp $ --
 
-CREATE temp TABLE import_id (
+CREATE TEMP TABLE import_id (
 email VARCHAR (64),
 id INTEGER,
 retire_to INTEGER
 );
 
-COPY import_id FROM '/tmp/id_import.out';
-
-DROP TABLE OGR_idlookup;
-
 CREATE TABLE OGR_idlookup (
 email VARCHAR (64),
 id INTEGER,
 stats_id INTEGER);
+
+TRUNCATE OGR_idlookup;
+DROP INDEX idlookup_id;
+DROP INDEX idlookup_email_lower;
+
+BEGIN;
+
+COPY import_id FROM '/tmp/id_import.out';
 
 INSERT INTO OGR_idlookup
 	SELECT email,
@@ -22,5 +26,9 @@ INSERT INTO OGR_idlookup
 			AS stats_id
 	FROM import_id;
 
+COMMIT;
+
 CREATE INDEX idlookup_id ON OGR_idlookup (id);
 CREATE UNIQUE INDEX idlookup_email_lower ON OGR_idlookup (lower(email));
+
+VACUUM ANALYZE OGR_idlookup;
