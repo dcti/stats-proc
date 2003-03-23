02bfc70419e116dbeb5a224edfabb1f9dc7dcfa9
decibel <decibel>
2003-03-23 23:18:12 +0000

    No need for ogr-verification in PGSQL branch

--- ogr-verification/donestubs.sql
+++
@@ -1,8 +0,0 @@
--- $Id: donestubs.sql,v 1.2 2002/12/23 18:44:06 nerf Exp $ --
-
-DROP TABLE donestubs;
-
-CREATE TABLE donestubs (
-stub_id INT,
-nodecount BIGINT,
-participants SMALLINT);
--- ogr-verification/initial.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# $Id: initial.sh,v 1.3 2002/12/21 21:13:22 joel Exp $
-#
-psql -d ogrstats -f create_stubs.sql
