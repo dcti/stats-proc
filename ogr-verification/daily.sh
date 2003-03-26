d1e5a9f0f174f5a68ab5dfe03edb39c2b3761648
nerf <nerf>
2003-03-26 22:24:07 +0000

    run stats.sql, to generate daily stats

--- ogr-verification/daily.sh
+++ ogr-verification/daily.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: daily.sh,v 1.12 2003/02/23 00:44:55 nerf Exp $
+# $Id: daily.sh,v 1.13 2003/03/26 22:24:07 nerf Exp $
 
 SQLUSER=$1
 SQLPASSWD=$2
@@ -9,3 +9,4 @@ psql ogr -a -U $SQLUSER -f create_id_lookup.sql &&
 rm -f /tmp/id_import.out
 psql ogr -a -U $SQLUSER -f movedata.sql &&
 psql ogr -a -U $SQLUSER -f summarize.sql &&
+psql ogr -a -U $SQLUSER -f stats.sql
