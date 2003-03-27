b6f453f48e6c103858df9cb2167860201ef3293b
nerf <nerf>
2003-03-27 22:41:59 +0000

    added brute force way of comverting to better format (thanks to CalicoJak)

--- ogr-verification/stats.sql
+++ ogr-verification/stats.sql
@@ -1,19 +1,28 @@
--- $Id: stats.sql,v 1.1 2003/03/20 18:40:09 nerf Exp $
+-- $Id: stats.sql,v 1.2 2003/03/27 22:41:59 nerf Exp $
 
 select now();
+
+CREATE TABLE public.ogr_complete (
+  rundate date DEFAULT ('now'::text)::date NOT NULL, 
+  project_id int2 NOT NULL, 
+  count int4 NOT NULL, 
+  pass1 int4 NOT NULL, 
+  pass2 int4 NOT NULL, 
+  CONSTRAINT ogr_complete_pkey PRIMARY KEY (rundate, project_id)
+) WITHOUT OIDS;
 
 BEGIN;
 
-delete from ogr_stats
-where  table_name = 'ogr_stubs'
-and function = 'count';
-
-insert into ogr_stats (table_name,function,result,project_id)
-select 'ogr_stubs','count',count(*),project_id
-from ogr_stubs
-group by project_id;
-
-select now();
+--delete from ogr_stats
+--where  table_name = 'ogr_stubs'
+--and function = 'count';
+--
+--insert into ogr_stats (table_name,function,result,project_id)
+--select 'ogr_stubs','count',count(*),project_id
+--from ogr_stubs
+--group by project_id;
+--
+--select now();
 
 
 delete from ogr_stats
@@ -38,6 +47,16 @@ where s.stub_id = su.stub_id
 	and su.participants >=2
 group by project_id;
 
+INSERT INTO ogr_complete
+SELECT CURRENT_DATE-1 as rundate,Cnt.project_id, Cnt.result as count, 
+        P1.result as Pass1, P2.result as Pass2
+FROM ogr_stats as Cnt, ogr_stats as P1, ogr_stats as P2 
+WHERE Cnt.function = 'count' AND P1.function = 'pass1'
+AND P2.function = 'pass2' AND Cnt.project_id = P1.project_id 
+AND P1.project_id = P2.project_id;
+
 COMMIT;
+
 select now();
+
 
