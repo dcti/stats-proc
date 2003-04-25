1be39173677ea13fecda17628d4536bac39c10b4
nerf <nerf>
2003-04-25 21:13:08 +0000

    drop/recreate index during/after large insert

--- ogr-verification/summarize.sql
+++ ogr-verification/summarize.sql
@@ -1,4 +1,4 @@
--- $Id: summarize.sql,v 1.1 2003/02/16 19:13:20 nerf Exp $
+-- $Id: summarize.sql,v 1.2 2003/04/25 21:13:08 nerf Exp $
 create table OGR_summary
 (
 	stub_id int,
@@ -9,6 +9,11 @@ create table OGR_summary
 
 truncate OGR_summary;
 
+CREATE INDEX results_id_count ON ogr_results USING btree (stub_id, nodecount);
+CREATE UNIQUE INDEX results_all
+	ON ogr_results USING btree (id, stub_id, nodecount, platform_id);
+
+DROP INDEX summ_maxpart;
 select now();
 insert into OGR_summary
 	select stub_id, nodecount,
@@ -18,3 +23,5 @@ insert into OGR_summary
 	AND R.platform_id = P.platform_id
 	group by stub_id, nodecount;
 select now();
+CREATE INDEX summ_maxpart on OGR_summary (max_client, participants);
+analyze ogr_summary;
