653e2ea86ba56d19bb22f01f34d559cd1e782295
nerf <nerf>
2003-04-25 21:10:57 +0000

    pass along different user/passwd for sybase vs pgsql pass along run date

--- ogr-verification/daily.sh
+++ ogr-verification/daily.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
-# $Id: daily.sh,v 1.13 2003/03/26 22:24:07 nerf Exp $
+# $Id: daily.sh,v 1.14 2003/04/25 21:10:57 nerf Exp $
 
-SQLUSER=$1
-SQLPASSWD=$2
+SYBUSER=$1
+SYBPASSWD=$2
+PGUSER=$3
+PGPASSWD=$4
+RUNDATE=$5
 
-sh get_idlookup.sh $SQLUSER $SQLPASSWD &&
-psql ogr -a -U $SQLUSER -f create_id_lookup.sql &&
+sh get_idlookup.sh $SYBUSER $SYBPASSWD &&
+psql ogr -a -U $PGUSER -f create_id_lookup.sql &&
 rm -f /tmp/id_import.out
-psql ogr -a -U $SQLUSER -f movedata.sql &&
-psql ogr -a -U $SQLUSER -f summarize.sql &&
-psql ogr -a -U $SQLUSER -f stats.sql
+psql ogr -a -U $PGUSER -f movedata.sql &&
+psql ogr -a -U $PGUSER -f summarize.sql &&
+psql ogr -a -U $PGUSER -v RUNDATE=\'$RUNDATE\' -f stats.sql
