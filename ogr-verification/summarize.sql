5ab65abdd0724a6ebfb696d0cd3f9d985c258cc3
nerf <nerf>
2003-05-03 12:25:07 +0000

    Formatting changes

--- ogr-verification/summarize.sql
+++ ogr-verification/summarize.sql
@@ -1,4 +1,4 @@
--- $Id: summarize.sql,v 1.2 2003/04/25 21:13:08 nerf Exp $
+-- $Id: summarize.sql,v 1.3 2003/05/03 12:25:07 nerf Exp $
 create table OGR_summary
 (
 	stub_id int,
@@ -7,21 +7,24 @@ create table OGR_summary
 	max_client int
 ) without oids;
 
-truncate OGR_summary;
-
 CREATE INDEX results_id_count ON ogr_results USING btree (stub_id, nodecount);
 CREATE UNIQUE INDEX results_all
 	ON ogr_results USING btree (id, stub_id, nodecount, platform_id);
 
 DROP INDEX summ_maxpart;
-select now();
-insert into OGR_summary
-	select stub_id, nodecount,
-		count(distinct I.stats_id), max(P.version)
-	from OGR_results R, OGR_idlookup I, platform P
-	where I.id = R.id
+TRUNCATE OGR_summary;
+
+SELECT now();
+INSERT INTO OGR_summary
+	SELECT stub_id, nodecount,
+		count(DISTINCT I.stats_id), max(P.version)
+	FROM OGR_results R, OGR_idlookup I, platform P
+	WHERE I.id = R.id
 	AND R.platform_id = P.platform_id
-	group by stub_id, nodecount;
-select now();
+	GROUP BY stub_id, nodecount;
+
+SELECT now();
+
 CREATE INDEX summ_maxpart on OGR_summary (max_client, participants);
-analyze ogr_summary;
+
+VACUUM ANALYZE ogr_summary;
