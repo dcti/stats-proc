ef40764868e1e38cc6f8e4d98d23aa1e65aa5f83
decibel <decibel>
2003-08-25 19:07:52 +0000

    Make test script more multi-project friendly

--- hourly/logmod/test-logmod.sh
+++ hourly/logmod/test-logmod.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
-./logmod -rc572 < test-input.rc572 > test-test
-cmp test-output.rc572 test-test
-rc=$?
-if [ $rc -ne 0 ]; then
-    echo "FAILED"
-    diff test-output.rc572 test-test
-    exit 1
-fi
-echo "PASSED"
+run_test () {
+    ./logmod -$1 < test-input.$1 > test-test
+    cmp test-output.$1 test-test
+    rc=$?
+    if [ $rc -ne 0 ]; then
+        echo "FAILED"
+        diff test-output.$1 test-test
+        exit 1
+    fi
+    echo "$1 PASSED"
+}
+
+run_test rc572
+
