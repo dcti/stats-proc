de6352e48eaf1962a26693d51aef158c795b1e38
nerf <nerf>
2003-09-02 14:08:02 +0000

    unique not working in 7.3.4. New table format for ogr_stubs

--- ogr-verification/create_all_stubs.sql
+++ ogr-verification/create_all_stubs.sql
@@ -1,32 +1,34 @@
--- $Id: create_all_stubs.sql,v 1.11 2003/05/13 14:05:42 nerf Exp $
-\set ON_ERROR_STOP 1
+-- $Id: create_all_stubs.sql,v 1.12 2003/09/02 14:08:02 nerf Exp $
 
-CREATE TEMPORARY TABLE OGR_stubs_import(
-stub_marks VARCHAR(22) not null);
-
-COPY OGR_stubs_import FROM '/home/nerf/all_stubs24';
+DROP TABLE OGR_stubs;
 
 DROP SEQUENCE OGR_stubs_stub_id_seq;
 
 CREATE TABLE OGR_stubs (
 	stub_marks VARCHAR(22) NOT NULL,
-	stub_id UNIQUE SERIAL,
+	stub_id SERIAL, -- UNIQUE not working ?
 	project_id SMALLINT NOT NULL
-);
+)WITHOUT OIDS;
 
-INSERT INTO OGR_stubs
-        SELECT stub_marks, NEXTVAL('OGR_stubs_stub_id_seq'),'24', 0, NULL, NULL
-FROM OGR_stubs_import ;
-
-DROP TABLE OGR_stubs_import ;
+\set ON_ERROR_STOP 1
 
 CREATE TEMPORARY TABLE OGR_stubs_import(
 stub_marks VARCHAR(22) not null);
 
-COPY OGR_stubs_import FROM '/home/nerf/all_stubs25';
+COPY OGR_stubs_import FROM
+'/home/nerf/dnet/stats-proc/ogr-verification/all_stubs25';
 
 INSERT INTO OGR_stubs
-        SELECT stub_marks, NEXTVAL('OGR_stubs_stub_id_seq'),'25', 0, NULL, NULL
+        SELECT stub_marks, NEXTVAL('OGR_stubs_stub_id_seq'),'25'
+FROM OGR_stubs_import ;
+
+TRUNCATE OGR_stubs_import ;
+
+COPY OGR_stubs_import FROM
+'/home/nerf/dnet/stats-proc/ogr-verification/all_stubs24';
+
+INSERT INTO OGR_stubs
+        SELECT stub_marks, NEXTVAL('OGR_stubs_stub_id_seq'),'24'
 FROM OGR_stubs_import ;
 
 CREATE UNIQUE INDEX stubs_marks ON OGR_stubs (stub_marks);
