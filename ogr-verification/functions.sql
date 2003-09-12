03bcd5231e14a1a998d4d6b2d732424aa498a96e
nerf <nerf>
2003-09-12 20:29:55 +0000

    formatting

--- ogr-verification/functions.sql
+++ ogr-verification/functions.sql
@@ -1,18 +1,21 @@
+-- $Id: functions.sql,v 1.3 2003/09/12 20:29:55 nerf Exp $
+-- General (and less general) functions
+
 CREATE OR REPLACE FUNCTION table_exists( VARCHAR ) RETURNS BOOLEAN AS '
 DECLARE
 t_name ALIAS for $1;
 t_result VARCHAR;
 BEGIN
 --find table, case-insensitive
-	SELECT relname INTO t_result
-		FROM pg_class
-		WHERE relname ~* (''^'' || t_name || ''$'')
-		AND relkind = ''r'';
-	IF t_result IS NULL THEN
-		RETURN FALSE;
-	ELSE 
-		RETURN TRUE;
-	END IF;
+  SELECT relname INTO t_result
+    FROM pg_class
+    WHERE relname ~* (''^'' || t_name || ''$'')
+    AND relkind = ''r'';
+  IF t_result IS NULL THEN
+    RETURN FALSE;
+  ELSE 
+    RETURN TRUE;
+  END IF;
 END;
 ' LANGUAGE 'plpgsql';
 
@@ -21,52 +24,52 @@ DECLARE
 TableName ALIAS FOR $1;
 T NAME;
 BEGIN
-	IF table_exists(TableName) THEN
-		T := lower(TableName);
-		EXECUTE ''DROP TABLE '' || quote_ident(T);
-		RETURN TRUE;
-	END IF;
-	RETURN FALSE;
+  IF table_exists(TableName) THEN
+    T := lower(TableName);
+    EXECUTE ''DROP TABLE '' || quote_ident(T);
+    RETURN TRUE;
+  END IF;
+  RETURN FALSE;
 END;
 ' LANGUAGE 'plpgsql' WITH (isstrict);
 
 CREATE OR REPLACE FUNCTION max(int4, int4) RETURNS int4 AS '
 BEGIN
-	IF $1 > $2 THEN
-		RETURN $1;
-	ELSE
-		RETURN $2;
-	END IF;
+  IF $1 > $2 THEN
+    RETURN $1;
+  ELSE
+    RETURN $2;
+  END IF;
 END;'  LANGUAGE 'plpgsql' IMMUTABLE STRICT;
 
 CREATE OR REPLACE FUNCTION min(int4, int4) RETURNS int4 AS '
 BEGIN
-	IF $1 < $2 THEN
-		RETURN $1;
-	ELSE
-		RETURN $2;
-	END IF;
+  IF $1 < $2 THEN
+    RETURN $1;
+  ELSE
+    RETURN $2;
+  END IF;
 END;'  LANGUAGE 'plpgsql' IMMUTABLE STRICT;
 
 CREATE OR REPLACE FUNCTION update_completed( VARCHAR, VARCHAR,DATE )
-	RETURNS BOOLEAN AS '
+  RETURNS BOOLEAN AS '
 DECLARE
 t_table_from ALIAS for $1;
 t_table_to ALIAS for $2;
 t_date ALIAS for $3;
 t_result VARCHAR;
 BEGIN
-	update ogr_completed
-	set t_field =
-	SELECT ogr_stats.t_field INTO t_result
-		FROM pg_class
-		WHERE relname ~* (''^'' || t_name || ''$'')
-		AND relkind = ''r'';
-	IF t_result IS NULL THEN
-		RETURN FALSE;
-	ELSE 
-		RETURN TRUE;
-	END IF;
+  update ogr_completed
+  set t_field =
+  SELECT ogr_stats.t_field INTO t_result
+    FROM pg_class
+    WHERE relname ~* (''^'' || t_name || ''$'')
+    AND relkind = ''r'';
+  IF t_result IS NULL THEN
+    RETURN FALSE;
+  ELSE 
+    RETURN TRUE;
+  END IF;
 END;
 ' LANGUAGE 'plpgsql';
 
@@ -79,14 +82,14 @@ END;
 -- and Tom Lane [tgl@sss.pgh.pa.us]
 
 create or replace function gmean_f1 (point, float) returns point as '
-  begin 
-    return point($1[0] * $2, $1[1] + 1); 
-  end' language plpgsql;
+begin 
+  return point($1[0] * $2, $1[1] + 1); 
+end' language plpgsql;
 
 create or replace function gmean_final (point) returns float as '
-  begin 
-    return ($1[0] ^ (1/($1[1]))); 
-  end' language plpgsql;
+begin 
+  return ($1[0] ^ (1/($1[1]))); 
+end' language plpgsql;
 
 create aggregate gmean(
   basetype=float, 
@@ -104,11 +107,11 @@ comment on function gmean_final(point) is 'Geometric mean aggregate final functi
 
 CREATE OR REPLACE FUNCTION rows() RETURNS int4 AS '
 DECLARE
-	f_rowcount int4;
+  f_rowcount int4;
 BEGIN
-	perform distinct * from log_fix;
-	GET DIAGNOSTICS f_rowcount = ROW_COUNT;
-	RETURN f_rowcount;
+  perform distinct * from log_fix;
+  GET DIAGNOSTICS f_rowcount = ROW_COUNT;
+  RETURN f_rowcount;
 END;'  LANGUAGE 'plpgsql' ;
 
 CREATE or replace FUNCTION stublen (varchar(22)) RETURNS integer AS '
