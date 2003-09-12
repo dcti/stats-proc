d15eb720c33b6ede8407a17ca670bbd6e3148fb5
nerf <nerf>
2003-09-12 20:34:25 +0000

    Multiple files have been merged into one.  This allows temp tables that are needed for the entire run to stay as temp tables instead of needing to be truncated when we're done with them.

--- ogr-verification/daily.sh
+++ ogr-verification/daily.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
-# $Id: daily.sh,v 1.17 2003/09/07 05:27:37 nerf Exp $
+# $Id: daily.sh,v 1.18 2003/09/12 20:34:25 nerf Exp $
 
 RUNDATE=$1
 
 rm -f /tmp/id_import.out &&
-psql ogr -a -v RUNDATE=\'$RUNDATE\' -f id_lookup.sql &&
-psql ogr -a -f movedata.sql &&
-psql ogr -a -f summarize.sql &&
+psql ogr -a -v RUNDATE=\'$RUNDATE\' -f daily_update.sql &&
 psql ogr -a -v RUNDATE=\'$RUNDATE\' -f stats.sql &&
 rm -f /tmp/id_import.out &&
-(
-for project_id in $(psql -q -t ogr -c 'select distinct project_id
-from ogr_complete')
-do
-sudo pcpages_ogr_verify pcpages $project_id 
-done
-)
+#(
+#for project_id in $(psql -q -t ogr -c 'select distinct project_id
+#from ogr_complete')
+#do
+#sudo pcpages_ogr_verify pcpages $project_id 
+#done
+#)
