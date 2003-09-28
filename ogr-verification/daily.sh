1d678c7cae060b16b9794327a89d9ddd732d8497
nerf <nerf>
2003-09-28 16:52:29 +0000

    stats generation is wrapped into dail_update.  uncomment post processing, but make it smarter

--- ogr-verification/daily.sh
+++ ogr-verification/daily.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
-# $Id: daily.sh,v 1.18 2003/09/12 20:34:25 nerf Exp $
+# $Id: daily.sh,v 1.19 2003/09/28 16:52:29 nerf Exp $
 
 RUNDATE=$1
 
 rm -f /tmp/id_import.out &&
 psql ogr -a -v RUNDATE=\'$RUNDATE\' -f daily_update.sql &&
-psql ogr -a -v RUNDATE=\'$RUNDATE\' -f stats.sql &&
 rm -f /tmp/id_import.out &&
-#(
-#for project_id in $(psql -q -t ogr -c 'select distinct project_id
-#from ogr_complete')
-#do
-#sudo pcpages_ogr_verify pcpages $project_id 
-#done
-#)
+(
+    if [ -f /usr/local/bin/pcpages_ogr_verify ]; then
+        for project_id in $(psql -q -t ogr -c
+            'select distinct project_id from ogr_complete')
+        do
+            sudo pcpages_ogr_verify pcpages $project_id 
+        done
+    fi
+)
