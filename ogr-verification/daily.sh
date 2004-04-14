189e5c60012b8a2f1b0e80306c29c47c7766de50
nerf <nerf>
2004-04-14 19:14:53 +0000

    clean up calls to post processing code

--- ogr-verification/daily.sh
+++ ogr-verification/daily.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
-# $Id: daily.sh,v 1.19 2003/09/28 16:52:29 nerf Exp $
+# $Id: daily.sh,v 1.20 2004/04/14 19:14:53 nerf Exp $
 
 RUNDATE=$1
+PCPAGES=/home/statproc/stats-proc/misc/pcpages_ogr_verify
 
 rm -f /tmp/id_import.out &&
 psql ogr -a -v RUNDATE=\'$RUNDATE\' -f daily_update.sql &&
 rm -f /tmp/id_import.out &&
 (
-    if [ -f /usr/local/bin/pcpages_ogr_verify ]; then
-        for project_id in $(psql -q -t ogr -c
+    if [ -f $PCPAGES ]; then
+        PROJECT_IDS=$(psql -q -t ogr -c \
             'select distinct project_id from ogr_complete')
-        do
-            sudo pcpages_ogr_verify pcpages $project_id 
-        done
+        sudo $PCPAGES pcpages $PROJECT_IDS 
     fi
 )
