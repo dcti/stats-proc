337895805a21b0b7de455e46c86c47259b874cf2
nerf <nerf>
2004-05-25 15:03:01 +0000

    Capture exit code instead of a string of statement with &&

--- ogr-verification/daily.sh
+++ ogr-verification/daily.sh
@@ -1,16 +1,21 @@
 #!/bin/sh
-# $Id: daily.sh,v 1.20 2004/04/14 19:14:53 nerf Exp $
+# $Id: daily.sh,v 1.21 2004/05/25 15:03:01 nerf Exp $
 
 RUNDATE=$1
 PCPAGES=/home/statproc/stats-proc/misc/pcpages_ogr_verify
 
 rm -f /tmp/id_import.out &&
-psql ogr -a -v RUNDATE=\'$RUNDATE\' -f daily_update.sql &&
-rm -f /tmp/id_import.out &&
+psql ogr -a -v RUNDATE=\'$RUNDATE\' -f daily_update.sql 
+RETURNSTATUS=$?
+rm -f /tmp/id_import.out
 (
     if [ -f $PCPAGES ]; then
         PROJECT_IDS=$(psql -q -t ogr -c \
             'select distinct project_id from ogr_complete')
-        sudo $PCPAGES pcpages $PROJECT_IDS 
+        sudo $PCPAGES pcpages $PROJECT_IDS
     fi
 )
+
+echo $RUNDATE `date` >> ./log/runtime
+
+return $RETURNSTATUS
