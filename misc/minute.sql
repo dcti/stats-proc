72dd4d9eec5a271e2e737a5b65c7bcbcfea2012c
decibel <decibel>
2005-04-22 16:37:29 +0000

    Fixes to log cleanup

--- misc/minute.sql
+++ misc/minute.sql
@@ -2,9 +2,12 @@
 DELETE
     FROM page_log.log
     WHERE (SELECT rrs.update()) >= 0
-        AND log_time < (SELECT min(last_end_time)
+        AND log_time < (SELECT min(coalesce(last_end_time, '1970-01-01 00:00:00-00'))
                             FROM rrs.source s
-                                JOIN rrs.source_status ss USING (source_id)
+                                JOIN rrs.rrs r ON ( 1 = 1 )
+                                LEFT JOIN rrs.source_status ss ON (s.source_id = ss.source_id
+                                                                    AND r.rrs_id = ss.rrs_id)
                             WHERE source_name = 'page_log'
+                                AND parent IS NULL
                         )
 ;
