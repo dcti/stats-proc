d3b775b0c655f8796ec4e96075c4e219d30359a8
nerf <nerf>
2005-04-29 20:15:58 +0000

    Break out tables by project

--- import_logs/initial_db.sql
+++ import_logs/initial_db.sql
@@ -1,51 +1,81 @@
--- $Id: initial_db.sql,v 1.3 2005/02/16 21:10:01 decibel Exp $
+-- $Id: initial_db.sql,v 1.4 2005/04/29 20:15:58 nerf Exp $
 
-CREATE TABLE log (
+CREATE TABLE email (
+	email_id	serial 		PRIMARY KEY
+	, email		varchar(64) 	NOT NULL UNIQUE
+) WITHOUT OIDs;
+
+CREATE TABLE platform (
+	platform_id	serial 	PRIMARY KEY
+	, os		int	NOT NULL
+	, cpu		int	NOT NULL
+	, version	int	NOT NULL
+	, UNIQUE (os, cpu, version)
+) WITHOUT OIDs;
+
+CREATE TABLE ogr_stubs (
+	ogr_stub_id		serial PRIMARY KEY
+	, stub_marks	charactervarying(22) NOT NULL UNIQUE
+) WITHOUT OIDS;
+
+CREATE TABLE log_8 (
 	return_time		timestamp NOT NULL
 	, ip_address		inet NOT NULL
-	, email_id		integer NOT NULL
-	, platform_id		integer NOT NULL
-	, project_id		integer NOT NULL
-	, ogr_stub_id		integer
+	, email_id		integer NOT NULL REFERENCES email
+	, platform_id		integer NOT NULL REFERENCES platform
 	, iter			integer
 	, cmc_count		integer
 	, cmc_ok		smallint
 	, core			smallint
-	, status		smallint
-	, nodecount		bigint
 	, key_block		charactervarying(20)
 	, cmc_last		charactervarying(20)
 ) WITHOUT OIDs;
 
-CREATE TABLE email (
-	email_id	SERIAL 		CONSTRAINT email__pk PRIMARY KEY
-	, email		varchar(64) 	NOT NULL CONSTRAINT email__email UNIQUE
+CREATE TABLE log_24 (
+	return_time		timestamp NOT NULL
+	, ip_address		inet NOT NULL
+	, email_id		integer NOT NULL REFERENCES email
+	, platform_id		integer NOT NULL REFERENCES platform
+	, ogr_stub_id		integer NOT NULL REFERENCES ogr_stubs
+	, nodecount		bigint
+	, status		smallint
 ) WITHOUT OIDs;
 
-CREATE TABLE platform (
-	platform_id	SERIAL 	CONSTRAINT platform__pk PRIMARY KEY
-	, os		int	NOT NULL
-	, cpu		int	NOT NULL
-	, version	int	NOT NULL
-	, CONSTRAINT platform__os_cpu_version UNIQUE
+CREATE TABLE log_25 (
+	return_time		timestamp NOT NULL
+	, ip_address		inet NOT NULL
+	, email_id		integer NOT NULL REFERENCES email
+	, platform_id		integer NOT NULL REFERENCES platform
+	, ogr_stub_id		integer NOT NULL REFERENCES ogr_stubs
+	, nodecount		bigint
+	, status		smallint
 ) WITHOUT OIDs;
 
-CREATE TABLE import (
+CREATE TABLE import_r72 (
 	return_time	timestamp NOT NULL
 	, ip_address	inet NOT NULL
 	, email		charactervarying(64) NOT NULL
 	, os_type	integer NOT NULL
 	, cpu_type	integer NOT NULL
 	, version	integer NOT NULL
-	, stub_marks	charactervarying(22)
-	, nodecount	bigint
-	, status	smallint
-	, key_block	charactervarying(20)
-	, iter		integer
-	, core		integer
-	, cmc_last	charactervarying(20)
-	, cmc_count	integer
-	, cmc_ok		smallint
+	, key_block	charactervarying(20) NOT NULL
+	, iter		integer NOT NULL
+	, core		integer NOT NULL
+	, cmc_last	charactervarying(20) NOT NULL
+	, cmc_count	integer NOT NULL
+	, cmc_ok		smallint NOT NULL
+) WITHOUT OIDs;
+
+CREATE TABLE import_ogr (
+	return_time	timestamp NOT NULL
+	, ip_address	inet NOT NULL
+	, email		charactervarying(64) NOT NULL
+	, os_type	integer NOT NULL
+	, cpu_type	integer NOT NULL
+	, version	integer NOT NULL
+	, stub_marks	charactervarying(22) NOT NULL
+	, nodecount	bigint NOT NULL
+	, status	smallint NOT NULL
 ) WITHOUT OIDs;
 
 -- vi: noexpandtab ts=8 sw=8
