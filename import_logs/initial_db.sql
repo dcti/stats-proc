d5a7c1e90ca53193648459dd3e3ff2c13ba78128
nerf <nerf>
2005-05-06 20:19:07 +0000

    s/charactervarying/varchar/

--- import_logs/initial_db.sql
+++ import_logs/initial_db.sql
@@ -1,4 +1,4 @@
--- $Id: initial_db.sql,v 1.5 2005/05/01 19:10:36 nerf Exp $
+-- $Id: initial_db.sql,v 1.6 2005/05/06 20:19:07 nerf Exp $
 
 CREATE TABLE email (
 	email_id	serial 		PRIMARY KEY
@@ -15,7 +15,7 @@ CREATE TABLE platform (
 
 CREATE TABLE ogr_stubs (
 	ogr_stub_id		serial PRIMARY KEY
-	, stub_marks	charactervarying(22) NOT NULL UNIQUE
+	, stub_marks	varchar(22) NOT NULL UNIQUE
 ) WITHOUT OIDS;
 
 CREATE TABLE log_8 (
@@ -23,12 +23,12 @@ CREATE TABLE log_8 (
 	, ip_address		inet NOT NULL
 	, email_id		integer NOT NULL REFERENCES email
 	, platform_id		integer NOT NULL REFERENCES platform
-	, iter			integer NOT NULL
-	, cmc_count		integer NOT NULL
-	, cmc_ok		smallint NOT NULL
+	, iter			smallint NOT NULL
+	, cmc_count		integer
+	, cmc_ok		smallint
 	, core			smallint NOT NULL
-	, key_block		charactervarying(20) NOT NULL
-	, cmc_last		charactervarying(20) NOT NULL
+	, key_block		varchar(20) NOT NULL
+	, cmc_last		varchar(20)
 ) WITHOUT OIDs;
 
 CREATE TABLE log_24 (
@@ -38,7 +38,7 @@ CREATE TABLE log_24 (
 	, platform_id		integer NOT NULL REFERENCES platform
 	, ogr_stub_id		integer NOT NULL REFERENCES ogr_stubs
 	, nodecount		bigint NOT NULL
-	, status		smallint NOT NULL
+	, status		smallint
 ) WITHOUT OIDs;
 
 CREATE TABLE log_25 (
@@ -48,34 +48,34 @@ CREATE TABLE log_25 (
 	, platform_id		integer NOT NULL REFERENCES platform
 	, ogr_stub_id		integer NOT NULL REFERENCES ogr_stubs
 	, nodecount		bigint NOT NULL
-	, status		smallint NOT NULL
+	, status		smallint
 ) WITHOUT OIDs;
 
 CREATE TABLE import_r72 (
 	return_time	timestamp NOT NULL
 	, ip_address	inet NOT NULL
-	, email		charactervarying(64) NOT NULL
+	, email		varchar(64) NOT NULL
 	, os_type	integer NOT NULL
 	, cpu_type	integer NOT NULL
 	, version	integer NOT NULL
-	, key_block	charactervarying(20) NOT NULL
-	, iter		integer NOT NULL
+	, key_block	varchar(20) NOT NULL
+	, iter		smallint NOT NULL
 	, core		integer NOT NULL
-	, cmc_last	charactervarying(20) NOT NULL
-	, cmc_count	integer NOT NULL
-	, cmc_ok		smallint NOT NULL
+	, cmc_last	varchar(20)
+	, cmc_count	integer
+	, cmc_ok	smallint
 ) WITHOUT OIDs;
 
 CREATE TABLE import_ogr (
 	return_time	timestamp NOT NULL
 	, ip_address	inet NOT NULL
-	, email		charactervarying(64) NOT NULL
+	, email		varchar(64) NOT NULL
 	, os_type	integer NOT NULL
 	, cpu_type	integer NOT NULL
 	, version	integer NOT NULL
-	, stub_marks	charactervarying(22) NOT NULL
+	, stub_marks	varchar(22) NOT NULL
 	, nodecount	bigint NOT NULL
-	, status	smallint NOT NULL
+	, status	smallint
 ) WITHOUT OIDs;
 
 -- vi: noexpandtab ts=8 sw=8
