ac7705bd03557121cac3ec398a1bec2af1d91927
decibel <decibel>
2009-02-25 06:50:40 +0000

    Don't use whatever arbitrary SSH auth socket we might have

--- misc/hourly.sh
+++ misc/hourly.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 #
-# $Id: hourly.sh,v 1.2 2005/05/10 23:12:56 decibel Exp $
+# $Id: hourly.sh,v 1.3 2009/02/25 06:50:40 decibel Exp $
+
+# We don't want to be trying to use some arbitrary auth info
+unset SSH_AUTH_SOCK
 
 cd ~/stats-proc/hourly && ./hourly.pl ~/workdir > ~/log/hourly.log  2> ~/log/hourly.err
